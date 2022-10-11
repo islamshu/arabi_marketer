f2c1a02644bae645e953f9803aef5653929ac09c
islamshu12 <islamshu12@gmail.com>
2022-10-11 11:55:58 +0300

    edit

--- app/Http/Controllers/Api/VideoController.php
+++ app/Http/Controllers/Api/VideoController.php
@@ -11,7 +11,9 @@ use App\Models\KeyWord;
 use App\Models\Video;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Api\BaseController;
-
+use App\Models\VideoCateogry;
+use App\Models\VideoKeyword;
+use Youtube;
 
 class VideoController extends BaseController
 {
@@ -35,5 +37,59 @@ class VideoController extends BaseController
        $service = Video::orderby('id','desc')->paginate(5);
        $res = VideoResource::collection($service)->response()->getData(true);
         return $this->sendResponse($res,'جميع الفيديوهات  ');
+    }
+    public function store(Request $request)
+    {
+                $vi = new Video();
+                $image = $request->image->store('video');
+                if($request->type_video == 'file'){
+                    $video = Youtube::upload($request->video->getPathName(), [
+                        'title'       => $request->title,
+                        'description' => $request->description,
+                    ])->withThumbnail($request->image->getPathName());
+                    $vi->url = "https://www.youtube.com/watch?v=" . $video->getVideoId();
+
+                }else{
+                    $vi->url = $request->url;
+                }
+                $vi->title = $request->title;
+                $vi->description = $request->description;
+                $vi->user_id =$request->user_id;
+                $vi->image = $image;
+                $vi->type = $request->type_video;
+
+                $vi->source = 'test';
+                $vi->save();
+                foreach ($request->types as $category) {
+                    $cat = new VideoCateogry();
+                    $cat->video_id = $vi->id;
+                    $cat->category_id = $category;
+                    $cat->save();
+                }
+
+                // dd($request->keywords);
+                $keywords = explode(',', $request->keywords);
+                foreach ($keywords as $s) {
+                    $keyword = KeyWord::ofType('podcast')->where('title', $s)->where('title', $s)->first();
+                    if ($keyword) {
+                        $key = new VideoKeyword();
+                        $key->video_id = $vi->id;
+                        $key->keyword_id = $keyword->id;
+                        $key->save();
+                    } else {
+
+                        $keyword = new KeyWord();
+                        $keyword->title = ['ar' => $s, 'en' => $s];
+                        $keyword->type = 'podcast';
+                        $keyword->save();
+
+                        $key = new VideoKeyword();
+                        $key->video_id = $vi->id;
+                        $key->keyword_id = $keyword->id;
+                        $key->save();
+                    }
+                }
+   
+        return redirect()->back();
     }
 }
