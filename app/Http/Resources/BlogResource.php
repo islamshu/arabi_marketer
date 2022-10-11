1d82ee11ac8318522c233efda78f806662fc39be
islamshu12 <islamshu12@gmail.com>
2022-10-11 17:22:40 +0300

    edit

--- app/Http/Resources/BlogResource.php
+++ app/Http/Resources/BlogResource.php
@@ -30,7 +30,7 @@ class BlogResource extends JsonResource
     }
     function get_related($data){
       $blogs =   Blog::where('id','!=',$this->id)->orderby('id','desc')->take(5)->get();
-      dd($blogs);
+      return BlogResource::collection($blogs);
     }
     function get_category($data){
         $category = $data->category;
