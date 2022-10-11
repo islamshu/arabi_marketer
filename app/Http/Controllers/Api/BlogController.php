70dbbffce25316c7952cefb74928b1d53f121ca1
islamshu12 <islamshu12@gmail.com>
2022-10-11 12:43:57 +0300

    edit

--- app/Http/Controllers/Api/BlogController.php
+++ app/Http/Controllers/Api/BlogController.php
@@ -99,8 +99,9 @@ class BlogController extends BaseController
         $service->image = $request->image->store('blog');
         $service->user_id = 3;
         $service->save();
-        return $request->all();
-        foreach ($request->category as $category) {
+
+        $categorys = explode(',', $request->category);
+        foreach ($categorys as $category) {
             $cat = new BlogCategory();
             $cat->blog_id = $service->id;
             $cat->category_id = $category;
