2d3240f5d191ccab6ba254bf49ae9f5c3b281b3a
islamshu12 <islamshu12@gmail.com>
2022-10-12 15:22:30 +0300

    edit

--- resources/views/pages/marketers/profile/show.blade.php
+++ resources/views/pages/marketers/profile/show.blade.php
@@ -311,24 +311,7 @@
             <!--end::Details-->
     
             <!--begin::Navs-->
-            <div class="d-flex overflow-auto h-55px">
-                <ul class="nav nav-stretch nav-line-tabs nav-line-tabs-2x border-transparent fs-5 fw-bolder flex-nowrap">
-                                    <!--begin::Nav item-->
-                        <li class="nav-item">
-                            <a class="nav-link text-active-primary me-6 " href="https://dashboard.arabicreators.com/account/overview">
-                                Overview
-                            </a>
-                        </li>
-                        <!--end::Nav item-->
-                                    <!--begin::Nav item-->
-                        <li class="nav-item">
-                            <a class="nav-link text-active-primary me-6 active" href="https://dashboard.arabicreators.com/account/settings">
-                                Settings
-                            </a>
-                        </li>
-                        <!--end::Nav item-->
-                                </ul>
-            </div>
+           
             <!--begin::Navs-->
         </div>
         <div class="card-body">
