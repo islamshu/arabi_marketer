b38826c45fa7f8729c83340f6cb260761a316c86
islamshu12 <islamshu12@gmail.com>
2022-10-12 15:25:32 +0300

    islam

--- resources/views/pages/marketers/profile/show.blade.php
+++ resources/views/pages/marketers/profile/show.blade.php
@@ -16,7 +16,7 @@
                 <!--begin: Pic-->
                 <div class="me-7 mb-4">
                     <div class="symbol symbol-100px symbol-lg-160px symbol-fixed position-relative">
-                        <img src="https://dashboard.arabicreators.com/demo1/media/avatars/blank.png" alt="image">
+                        <img src="{{ asset('public/uploads/'.$user->image) }}" alt="image">
                         <div
                             class="position-absolute translate-middle bottom-0 start-100 mb-6 bg-success rounded-circle border border-4 border-white h-20px w-20px">
                         </div>
@@ -32,7 +32,7 @@
                         <div class="d-flex flex-column">
                             <!--begin::Name-->
                             <div class="d-flex align-items-center mb-2">
-                                <a href="#" class="text-gray-800 text-hover-primary fs-2 fw-bolder me-1">islam</a>
+                                <a href="#" class="text-gray-800 text-hover-primary fs-2 fw-bolder me-1">{{ $user->name }}</a>
                                 <a href="#">
                                     <!--begin::Svg Icon | path: assets/media/icons/duotune/general/gen026.svg-->
                                     <span class="svg-icon svg-icon-1 svg-icon-primary"><svg
@@ -48,8 +48,6 @@
                                     <!--end::Svg Icon-->
                                 </a>
 
-                                <a href="#" class="btn btn-sm btn-light-success fw-bolder ms-2 fs-8 py-1 px-3"
-                                    data-bs-toggle="modal" data-bs-target="#kt_modal_upgrade_plan">Upgrade to Pro</a>
                             </div>
                             <!--end::Name-->
 
@@ -70,23 +68,9 @@
                                                 rx="2" fill="currentColor"></rect>
                                         </svg></span>
                                     <!--end::Svg Icon-->
-                                    Developer
+                                    {{ $user->types }}
                                 </a>
-                                <a href="#"
-                                    class="d-flex align-items-center text-gray-400 text-hover-primary me-5 mb-2">
-                                    <!--begin::Svg Icon | path: assets/media/icons/duotune/general/gen018.svg-->
-                                    <span class="svg-icon svg-icon-4 me-1"><svg xmlns="http://www.w3.org/2000/svg"
-                                            width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                            <path opacity="0.3"
-                                                d="M18.0624 15.3453L13.1624 20.7453C12.5624 21.4453 11.5624 21.4453 10.9624 20.7453L6.06242 15.3453C4.56242 13.6453 3.76242 11.4453 4.06242 8.94534C4.56242 5.34534 7.46242 2.44534 11.0624 2.04534C15.8624 1.54534 19.9624 5.24534 19.9624 9.94534C20.0624 12.0453 19.2624 13.9453 18.0624 15.3453Z"
-                                                fill="currentColor"></path>
-                                            <path
-                                                d="M12.0624 13.0453C13.7193 13.0453 15.0624 11.7022 15.0624 10.0453C15.0624 8.38849 13.7193 7.04535 12.0624 7.04535C10.4056 7.04535 9.06241 8.38849 9.06241 10.0453C9.06241 11.7022 10.4056 13.0453 12.0624 13.0453Z"
-                                                fill="currentColor"></path>
-                                        </svg></span>
-                                    <!--end::Svg Icon-->
-                                    SF, Bay Area
-                                </a>
+                             
                                 <a href="#"
                                     class="d-flex align-items-center text-gray-400 text-hover-primary mb-2">
                                     <!--begin::Svg Icon | path: assets/media/icons/duotune/communication/com011.svg-->
@@ -100,7 +84,7 @@
                                                 fill="currentColor"></path>
                                         </svg></span>
                                     <!--end::Svg Icon-->
-                                    info@arabicreators.com
+                                   {{ $user->email }}
                                 </a>
                             </div>
                             <!--end::Info-->
@@ -108,167 +92,7 @@
                         <!--end::User-->
 
                         <!--begin::Actions-->
-                        <div class="d-flex my-4">
-                            <a href="#" class="btn btn-sm btn-light me-2" id="kt_user_follow_button">
-                                <!--begin::Svg Icon | path: assets/media/icons/duotune/arrows/arr012.svg-->
-                                <span class="svg-icon svg-icon-3 d-none"><svg xmlns="http://www.w3.org/2000/svg"
-                                        width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                        <path opacity="0.3"
-                                            d="M10 18C9.7 18 9.5 17.9 9.3 17.7L2.3 10.7C1.9 10.3 1.9 9.7 2.3 9.3C2.7 8.9 3.29999 8.9 3.69999 9.3L10.7 16.3C11.1 16.7 11.1 17.3 10.7 17.7C10.5 17.9 10.3 18 10 18Z"
-                                            fill="currentColor"></path>
-                                        <path
-                                            d="M10 18C9.7 18 9.5 17.9 9.3 17.7C8.9 17.3 8.9 16.7 9.3 16.3L20.3 5.3C20.7 4.9 21.3 4.9 21.7 5.3C22.1 5.7 22.1 6.30002 21.7 6.70002L10.7 17.7C10.5 17.9 10.3 18 10 18Z"
-                                            fill="currentColor"></path>
-                                    </svg></span>
-                                <!--end::Svg Icon-->
-                                <!--begin::Indicator-->
-                                <span class="indicator-label">
-                                    Follow
-                                </span>
-                                <span class="indicator-progress">
-                                    Please wait...
-                                    <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
-                                </span>
-                                <!--end::Indicator-->
-
-                            </a>
-
-                            <a href="#" class="btn btn-sm btn-primary me-3" data-bs-toggle="tooltip"
-                                data-bs-placement="left" data-bs-trigger="hover" data-kt-initialized="1">Hire Me</a>
-
-                            <!--begin::Menu-->
-                            <div class="me-0">
-                                <button class="btn btn-sm btn-icon btn-bg-light btn-active-color-primary"
-                                    data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
-                                    <i class="bi bi-three-dots fs-3"></i>
-                                </button>
-                                <!--begin::Menu 3-->
-                                <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-bold w-200px py-3"
-                                    data-kt-menu="true">
-                                    <!--begin::Heading-->
-                                    <div class="menu-item px-3">
-                                        <div class="menu-content text-muted pb-2 px-3 fs-7 text-uppercase">
-                                            Payments
-                                        </div>
-                                    </div>
-                                    <!--end::Heading-->
-
-                                    <!--begin::Menu item-->
-                                    <div class="menu-item px-3" data-bs-toggle="tooltip" data-bs-placement="left"
-                                        data-bs-trigger="hover" data-kt-initialized="1">
-                                        <a href="#" class="menu-link px-3">
-                                            Create Invoice
-                                        </a>
-                                    </div>
-                                    <!--end::Menu item-->
-
-                                    <!--begin::Menu item-->
-                                    <div class="menu-item px-3" data-bs-toggle="tooltip" data-bs-placement="left"
-                                        data-bs-trigger="hover" data-kt-initialized="1">
-                                        <a href="#" class="menu-link flex-stack px-3">
-                                            Create Payment
-
-                                            <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip"
-                                                aria-label="Specify a target name for future usage and reference"
-                                                data-kt-initialized="1"></i>
-                                        </a>
-                                    </div>
-                                    <!--end::Menu item-->
-
-                                    <!--begin::Menu item-->
-                                    <div class="menu-item px-3" data-bs-toggle="tooltip" data-bs-placement="left"
-                                        data-bs-trigger="hover" data-kt-initialized="1">
-                                        <a href="#" class="menu-link px-3">
-                                            Generate Bill
-                                        </a>
-                                    </div>
-                                    <!--end::Menu item-->
-
-                                    <!--begin::Menu item-->
-                                    <div class="menu-item px-3" data-kt-menu-trigger="hover"
-                                        data-kt-menu-placement="left-start" data-bs-toggle="tooltip"
-                                        data-bs-placement="left" data-bs-trigger="hover" data-kt-initialized="1">
-                                        <a href="#" class="menu-link px-3">
-                                            <span class="menu-title">Subscription</span>
-                                            <span class="menu-arrow"></span>
-                                        </a>
-
-                                        <!--begin::Menu sub-->
-                                        <div class="menu-sub menu-sub-dropdown w-175px py-4">
-                                            <!--begin::Menu item-->
-                                            <div class="menu-item px-3" data-bs-toggle="tooltip"
-                                                data-bs-placement="left" data-bs-trigger="hover"
-                                                data-kt-initialized="1">
-                                                <a href="#" class="menu-link px-3">
-                                                    Plans
-                                                </a>
-                                            </div>
-                                            <!--end::Menu item-->
-
-                                            <!--begin::Menu item-->
-                                            <div class="menu-item px-3" data-bs-toggle="tooltip"
-                                                data-bs-placement="left" data-bs-trigger="hover"
-                                                data-kt-initialized="1">
-                                                <a href="#" class="menu-link px-3">
-                                                    Billing
-                                                </a>
-                                            </div>
-                                            <!--end::Menu item-->
-
-                                            <!--begin::Menu item-->
-                                            <div class="menu-item px-3" data-bs-toggle="tooltip"
-                                                data-bs-placement="left" data-bs-trigger="hover"
-                                                data-kt-initialized="1">
-                                                <a href="#" class="menu-link px-3">
-                                                    Statements
-                                                </a>
-                                            </div>
-                                            <!--end::Menu item-->
-
-                                            <!--begin::Menu separator-->
-                                            <div class="separator my-2"></div>
-                                            <!--end::Menu separator-->
-
-                                            <!--begin::Menu item-->
-                                            <div class="menu-item px-3">
-                                                <div class="menu-content px-3">
-                                                    <!--begin::Switch-->
-                                                    <label
-                                                        class="form-check form-switch form-check-custom form-check-solid">
-                                                        <!--begin::Input-->
-                                                        <input class="form-check-input w-30px h-20px" type="checkbox"
-                                                            value="1" checked="checked" name="notifications">
-                                                        <!--end::Input-->
-
-                                                        <!--end::Label-->
-                                                        <span class="form-check-label text-muted fs-6">
-                                                            Recurring
-                                                        </span>
-                                                        <!--end::Label-->
-                                                    </label>
-                                                    <!--end::Switch-->
-                                                </div>
-                                            </div>
-                                            <!--end::Menu item-->
-                                        </div>
-                                        <!--end::Menu sub-->
-                                    </div>
-                                    <!--end::Menu item-->
-
-                                    <!--begin::Menu item-->
-                                    <div class="menu-item px-3 my-1">
-                                        <a href="https://dashboard.arabicreators.com/account/settings"
-                                            class="menu-link px-3">
-                                            Settings
-                                        </a>
-                                    </div>
-                                    <!--end::Menu item-->
-                                </div>
-                                <!--end::Menu 3-->
-
-                            </div>
-                            <!--end::Menu-->
-                        </div>
+                       
                         <!--end::Actions-->
                     </div>
                     <!--end::Title-->
