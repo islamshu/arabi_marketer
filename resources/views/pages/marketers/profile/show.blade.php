f24248abe0216366ef77f289a0e5dda059a1f624
islamshu12 <islamshu12@gmail.com>
2022-10-12 15:31:49 +0300

    edit

--- resources/views/pages/marketers/profile/show.blade.php
+++ resources/views/pages/marketers/profile/show.blade.php
@@ -175,6 +175,9 @@
                 <!--end::Info-->
             </div>
             
+            
+        </div>
+        <div class="card-body">
             <ul class="nav nav-tabs nav-line-tabs nav-line-tabs-2x mb-5 fs-6">
                 <li class="nav-item">
                     <a class="nav-link active" data-bs-toggle="tab" href="#kt_tab_pane_4">الخدمات <span
