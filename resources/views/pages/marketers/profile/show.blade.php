f738de32f39899a4ac43ea4f05f510ce149c564e
islamshu12 <islamshu12@gmail.com>
2022-10-12 15:44:29 +0300

    edit

--- resources/views/pages/marketers/profile/show.blade.php
+++ resources/views/pages/marketers/profile/show.blade.php
@@ -1,11 +1,13 @@
 @extends('layout.main')
 <style>
     .count {
-        background: black;
+        background: #689258;
         border-radius: 21px;
         padding: 6px;
+        color: beige;
     }
-    .cur{
+
+    .cur {
         font-size: 15px
     }
 </style>
@@ -19,8 +21,8 @@
                 <!--begin: Pic-->
                 <div class="me-7 mb-4">
                     <div class="symbol symbol-100px symbol-lg-160px symbol-fixed position-relative">
-                        <img src="{{ asset('public/uploads/'.$user->image) }}" alt="image">
-                 
+                        <img src="{{ asset('public/uploads/' . $user->image) }}" alt="image">
+
                     </div>
                 </div>
                 <!--end::Pic-->
@@ -33,21 +35,22 @@
                         <div class="d-flex flex-column">
                             <!--begin::Name-->
                             <div class="d-flex align-items-center mb-2">
-                                <a href="#" class="text-gray-800 text-hover-primary fs-2 fw-bolder me-1">{{ $user->name }}</a>
+                                <a href="#"
+                                    class="text-gray-800 text-hover-primary fs-2 fw-bolder me-1">{{ $user->name }}</a>
                                 <a href="#">
                                     <!--begin::Svg Icon | path: assets/media/icons/duotune/general/gen026.svg-->
-                                    @if($user->status == 1)
-                                    <span class="svg-icon svg-icon-1 svg-icon-primary"><svg
-                                            xmlns="http://www.w3.org/2000/svg" width="24px" height="24px"
-                                            viewBox="0 0 24 24">
-                                            <path
-                                                d="M10.0813 3.7242C10.8849 2.16438 13.1151 2.16438 13.9187 3.7242V3.7242C14.4016 4.66147 15.4909 5.1127 16.4951 4.79139V4.79139C18.1663 4.25668 19.7433 5.83365 19.2086 7.50485V7.50485C18.8873 8.50905 19.3385 9.59842 20.2758 10.0813V10.0813C21.8356 10.8849 21.8356 13.1151 20.2758 13.9187V13.9187C19.3385 14.4016 18.8873 15.491 19.2086 16.4951V16.4951C19.7433 18.1663 18.1663 19.7433 16.4951 19.2086V19.2086C15.491 18.8873 14.4016 19.3385 13.9187 20.2758V20.2758C13.1151 21.8356 10.8849 21.8356 10.0813 20.2758V20.2758C9.59842 19.3385 8.50905 18.8873 7.50485 19.2086V19.2086C5.83365 19.7433 4.25668 18.1663 4.79139 16.4951V16.4951C5.1127 15.491 4.66147 14.4016 3.7242 13.9187V13.9187C2.16438 13.1151 2.16438 10.8849 3.7242 10.0813V10.0813C4.66147 9.59842 5.1127 8.50905 4.79139 7.50485V7.50485C4.25668 5.83365 5.83365 4.25668 7.50485 4.79139V4.79139C8.50905 5.1127 9.59842 4.66147 10.0813 3.7242V3.7242Z"
-                                                fill="currentColor"></path>
-                                            <path
-                                                d="M14.8563 9.1903C15.0606 8.94984 15.3771 8.9385 15.6175 9.14289C15.858 9.34728 15.8229 9.66433 15.6185 9.9048L11.863 14.6558C11.6554 14.9001 11.2876 14.9258 11.048 14.7128L8.47656 12.4271C8.24068 12.2174 8.21944 11.8563 8.42911 11.6204C8.63877 11.3845 8.99996 11.3633 9.23583 11.5729L11.3706 13.4705L14.8563 9.1903Z"
-                                                fill="white"></path>
-                                        </svg></span>
-                                        @endif
+                                    @if ($user->status == 1)
+                                        <span class="svg-icon svg-icon-1 svg-icon-primary"><svg
+                                                xmlns="http://www.w3.org/2000/svg" width="24px" height="24px"
+                                                viewBox="0 0 24 24">
+                                                <path
+                                                    d="M10.0813 3.7242C10.8849 2.16438 13.1151 2.16438 13.9187 3.7242V3.7242C14.4016 4.66147 15.4909 5.1127 16.4951 4.79139V4.79139C18.1663 4.25668 19.7433 5.83365 19.2086 7.50485V7.50485C18.8873 8.50905 19.3385 9.59842 20.2758 10.0813V10.0813C21.8356 10.8849 21.8356 13.1151 20.2758 13.9187V13.9187C19.3385 14.4016 18.8873 15.491 19.2086 16.4951V16.4951C19.7433 18.1663 18.1663 19.7433 16.4951 19.2086V19.2086C15.491 18.8873 14.4016 19.3385 13.9187 20.2758V20.2758C13.1151 21.8356 10.8849 21.8356 10.0813 20.2758V20.2758C9.59842 19.3385 8.50905 18.8873 7.50485 19.2086V19.2086C5.83365 19.7433 4.25668 18.1663 4.79139 16.4951V16.4951C5.1127 15.491 4.66147 14.4016 3.7242 13.9187V13.9187C2.16438 13.1151 2.16438 10.8849 3.7242 10.0813V10.0813C4.66147 9.59842 5.1127 8.50905 4.79139 7.50485V7.50485C4.25668 5.83365 5.83365 4.25668 7.50485 4.79139V4.79139C8.50905 5.1127 9.59842 4.66147 10.0813 3.7242V3.7242Z"
+                                                    fill="currentColor"></path>
+                                                <path
+                                                    d="M14.8563 9.1903C15.0606 8.94984 15.3771 8.9385 15.6175 9.14289C15.858 9.34728 15.8229 9.66433 15.6185 9.9048L11.863 14.6558C11.6554 14.9001 11.2876 14.9258 11.048 14.7128L8.47656 12.4271C8.24068 12.2174 8.21944 11.8563 8.42911 11.6204C8.63877 11.3845 8.99996 11.3633 9.23583 11.5729L11.3706 13.4705L14.8563 9.1903Z"
+                                                    fill="white"></path>
+                                            </svg></span>
+                                    @endif
                                     <!--end::Svg Icon-->
                                 </a>
 
@@ -73,7 +76,7 @@
                                     <!--end::Svg Icon-->
                                     {{ $user->type }}
                                 </a>
-                             
+
                                 <a href="#"
                                     class="d-flex align-items-center text-gray-400 text-hover-primary mb-2">
                                     <!--begin::Svg Icon | path: assets/media/icons/duotune/communication/com011.svg-->
@@ -87,7 +90,7 @@
                                                 fill="currentColor"></path>
                                         </svg></span>
                                     <!--end::Svg Icon-->
-                                   {{ $user->email }}
+                                    {{ $user->email }}
                                 </a>
                             </div>
                             <!--end::Info-->
@@ -95,7 +98,7 @@
                         <!--end::User-->
 
                         <!--begin::Actions-->
-                       
+
                         <!--end::Actions-->
                     </div>
                     <!--end::Title-->
@@ -112,11 +115,12 @@
                                     <!--begin::Number-->
                                     <div class="d-flex align-items-center">
                                         <!--begin::Svg Icon | path: assets/media/icons/duotune/arrows/arr066.svg-->
-                                       
+
                                         <!--end::Svg Icon-->
                                         <div class="fs-2 fw-bolder counted" data-kt-countup="true"
                                             data-kt-countup-value="4500" data-kt-countup-prefix="$"
-                                            data-kt-initialized="1"><span class="cur"> ريال </span>{{ $user->total }}</div>
+                                            data-kt-initialized="1"><span class="cur"> ريال
+                                            </span>{{ $user->total }}</div>
                                     </div>
                                     <!--end::Number-->
 
@@ -132,10 +136,11 @@
                                     <!--begin::Number-->
                                     <div class="d-flex align-items-center">
                                         <!--begin::Svg Icon | path: assets/media/icons/duotune/arrows/arr065.svg-->
-                                      
+
                                         <!--end::Svg Icon-->
                                         <div class="fs-2 fw-bolder counted" data-kt-countup="true"
-                                            data-kt-countup-value="75" data-kt-initialized="1"><span class="cur"> ريال </span>{{ $user->available }}</div>
+                                            data-kt-countup-value="75" data-kt-initialized="1"><span class="cur">
+                                                ريال </span>{{ $user->available }}</div>
                                     </div>
                                     <!--end::Number-->
 
@@ -151,11 +156,12 @@
                                     <!--begin::Number-->
                                     <div class="d-flex align-items-center">
                                         <!--begin::Svg Icon | path: assets/media/icons/duotune/arrows/arr066.svg-->
-                                       
+
                                         <!--end::Svg Icon-->
                                         <div class="fs-2 fw-bolder counted" data-kt-countup="true"
                                             data-kt-countup-value="60" data-kt-countup-prefix="%"
-                                            data-kt-initialized="1"><span class="cur"> ريال </span>{{ $user->pending }} </div>
+                                            data-kt-initialized="1"><span class="cur"> ريال
+                                            </span>{{ $user->pending }} </div>
                                     </div>
                                     <!--end::Number-->
 
@@ -175,8 +181,8 @@
                 </div>
                 <!--end::Info-->
             </div>
-            
-            
+
+
         </div>
         <div class="card-body">
             <ul class="nav nav-tabs nav-line-tabs nav-line-tabs-2x mb-5 fs-6">
@@ -224,7 +230,7 @@
 
             </div>
         </div>
-      
+
         <!--end::Card body-->
     </div>
 
