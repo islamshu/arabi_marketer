9d3ca8486d47887f340f22e2a28365d84f26d03a
islamshu12 <islamshu12@gmail.com>
2022-10-15 14:33:13 +0300

    edit

--- resources/views/mail/rest.blade.php
+++ resources/views/mail/rest.blade.php
@@ -4,13 +4,17 @@
     You are receiving this email because we received a password reset request for your account.
     Reset Password 
     
+</p>
     <a href="{{ $url }}">Reset Password</a>
+    <p>
     
     
     This password reset link will expire in 60 minutes.
     If you did not request a password reset, no further action is required.
     Regards,
-    Laravel
+    Laravel.
+</p>
+    copy link : {{ $url }}
 </p>
 
 </div>
