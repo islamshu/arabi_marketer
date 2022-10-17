15da2e99dfa4f59129f1acd9fbc1e60a15a8a669
islamshu12 <islamshu12@gmail.com>
2022-10-17 13:41:56 +0300

    edit

--- app/Http/Controllers/Api/ConsultationController.php
+++ app/Http/Controllers/Api/ConsultationController.php
@@ -93,9 +93,7 @@ class ConsultationController extends BaseController
             return $q->where('title','like','%'.$title.'%');
         });
         $query->when($request->category_id !=null, function ($q) use ($request) {
-            return $q->has('type')->with(['type' => function ($query) use ($request) {
-                $query->where('category_id', $request->category_id);
-            }]);
+            return $q->where('type_id',$request->category_id);
         });
 
        
