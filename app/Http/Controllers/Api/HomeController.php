fe4c1bf773e887d732640ce16b03e024272c747f
islamshu12 <islamshu12@gmail.com>
2022-10-18 15:17:29 +0300

    edit

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -44,7 +44,7 @@ class HomeController extends BaseController
         $res['service']['best'] = $services;
 
         $blogs = BlogResource::collection(Blog::orderBy('id', 'desc')->take(6)->get());
-        $res['blog_categotry']= CategoryBlogResource::collection(Category::ofType('blog')->get());
+        // $res['blog_categotry']= CategoryBlogResource::collection(Category::ofType('blog')->get());
         $res['blog']=CategoryBlogResource::collection(Category::ofType('blog')->get());
 
         $res['blog']['new'] = $blogs;
