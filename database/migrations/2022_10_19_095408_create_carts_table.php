0c8a834ddd1de5347dd3a7c77f044e78cbf1adca
islamshu12 <islamshu12@gmail.com>
2022-10-19 12:59:03 +0300

    edit

--- database/migrations/2022_10_19_095408_create_carts_table.php
+++ database/migrations/2022_10_19_095408_create_carts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
-            $table->unsignedInteger('user_id');
+            $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('owner_id');
             $table->unsignedBigInteger('service_id');
             $table->string('price');
