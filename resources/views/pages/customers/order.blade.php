bda1236c24fdd27111617873757ca911ded953f2
islamshu12 <islamshu12@gmail.com>
2022-10-20 14:46:42 +0300

    e

--- resources/views/pages/customers/order.blade.php
+++ resources/views/pages/customers/order.blade.php
@@ -8,23 +8,49 @@
             <div class="d-flex flex-column gap-7 gap-lg-10">
                 <div class="d-flex flex-wrap flex-stack gap-5 gap-lg-10">
                     <!--begin:::Tabs-->
-                    <ul class="nav nav-custom nav-tabs nav-line-tabs nav-line-tabs-2x border-0 fs-4 fw-semibold mb-lg-n2 me-auto"
-                        role="tablist">
-                        <!--begin:::Tab item-->
-                        <li class="nav-item" role="presentation">
-                            <a class="nav-link text-active-primary pb-4 active" data-bs-toggle="tab"
-                                href="#kt_ecommerce_sales_order_summary" aria-selected="true" role="tab">Order
-                                Summary</a>
-                        </li>
-                        <!--end:::Tab item-->
-                        <!--begin:::Tab item-->
-                        <li class="nav-item" role="presentation">
-                            <a class="nav-link text-active-primary pb-4" data-bs-toggle="tab"
-                                href="#kt_ecommerce_sales_order_history" aria-selected="false" tabindex="-1"
-                                role="tab">Order History</a>
-                        </li>
-                        <!--end:::Tab item-->
-                    </ul>
+                    <ul class="nav nav-tabs nav-line-tabs nav-line-tabs-2x border-transparent fs-4 fw-semibold mb-15"
+                    role="tablist">
+                    <!--begin:::Tab item-->
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link text-active-primary pb-5 " href="/">
+                            <!--begin::Svg Icon | path: icons/duotune/general/gen001.svg-->
+                            <span class="svg-icon svg-icon-2 me-2">
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M11 2.375L2 9.575V20.575C2 21.175 2.4 21.575 3 21.575H9C9.6 21.575 10 21.175 10 20.575V14.575C10 13.975 10.4 13.575 11 13.575H13C13.6 13.575 14 13.975 14 14.575V20.575C14 21.175 14.4 21.575 15 21.575H21C21.6 21.575 22 21.175 22 20.575V9.575L13 2.375C12.4 1.875 11.6 1.875 11 2.375Z"
+                                        fill="currentColor"></path>
+                                </svg>
+                            </span>
+                            <!--end::Svg Icon-->الرئيسية
+                        </a>
+    
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link text-active-primary pb-5 active">
+                            <!--begin::Svg Icon | path: icons/duotune/general/gen001.svg-->
+                            <span class="svg-icon svg-icon-2 me-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none">
+                                    <path opacity="0.3"
+                                        d="M19 22H5C4.4 22 4 21.6 4 21V3C4 2.4 4.4 2 5 2H14L20 8V21C20 21.6 19.6 22 19 22ZM12.5 18C12.5 17.4 12.6 17.5 12 17.5H8.5C7.9 17.5 8 17.4 8 18C8 18.6 7.9 18.5 8.5 18.5L12 18C12.6 18 12.5 18.6 12.5 18ZM16.5 13C16.5 12.4 16.6 12.5 16 12.5H8.5C7.9 12.5 8 12.4 8 13C8 13.6 7.9 13.5 8.5 13.5H15.5C16.1 13.5 16.5 13.6 16.5 13ZM12.5 8C12.5 7.4 12.6 7.5 12 7.5H8C7.4 7.5 7.5 7.4 7.5 8C7.5 8.6 7.4 8.5 8 8.5H12C12.6 8.5 12.5 8.6 12.5 8Z"
+                                        fill="currentColor"></path>
+                                    <rect x="7" y="17" width="6" height="2" rx="1"
+                                        fill="currentColor"></rect>
+                                    <rect x="7" y="12" width="10" height="2" rx="1"
+                                        fill="currentColor"></rect>
+                                    <rect x="7" y="7" width="6" height="2" rx="1"
+                                        fill="currentColor"></rect>
+                                    <path d="M15 8H20L14 2V7C14 7.6 14.4 8 15 8Z" fill="currentColor"></path>
+                                </svg>
+                            </span>
+                            <!--end::Svg Icon-->الطلب {{ $order->code }}  
+                        </a>
+    
+                    </li>
+    
+    
+                </ul>
                     <!--end:::Tabs-->
                     <!--begin::Button-->
                     <a href="/metronic8/demo1/../demo1/apps/ecommerce/sales/listing.html"
