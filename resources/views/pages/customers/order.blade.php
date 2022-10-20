19848b2c7ff766843086c2d668cf6b340f5c0084
islamshu12 <islamshu12@gmail.com>
2022-10-20 14:48:49 +0300

    j

--- resources/views/pages/customers/order.blade.php
+++ resources/views/pages/customers/order.blade.php
@@ -51,23 +51,7 @@
     
     
                 </ul>
-                    <!--end:::Tabs-->
-                    <!--begin::Button-->
-                    <a href="/metronic8/demo1/../demo1/apps/ecommerce/sales/listing.html"
-                        class="btn btn-icon btn-light btn-sm ms-auto me-lg-n7">
-                        <!--begin::Svg Icon | path: icons/duotune/arrows/arr074.svg-->
-                        <span class="svg-icon svg-icon-2">
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M11.2657 11.4343L15.45 7.25C15.8642 6.83579 15.8642 6.16421 15.45 5.75C15.0358 5.33579 14.3642 5.33579 13.95 5.75L8.40712 11.2929C8.01659 11.6834 8.01659 12.3166 8.40712 12.7071L13.95 18.25C14.3642 18.6642 15.0358 18.6642 15.45 18.25C15.8642 17.8358 15.8642 17.1642 15.45 16.75L11.2657 12.5657C10.9533 12.2533 10.9533 11.7467 11.2657 11.4343Z"
-                                    fill="currentColor"></path>
-                            </svg>
-                        </span>
-                        <!--end::Svg Icon-->
-                    </a>
-                    <!--end::Button-->
-                    <!--begin::Button-->
+                    
 
                 </div>
                 <!--begin::Order summary-->
