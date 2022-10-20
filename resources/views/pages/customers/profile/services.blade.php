2f3765f63c14ae8b7be4800a90b9f66bc158761e
islamshu12 <islamshu12@gmail.com>
2022-10-20 13:34:24 +0300

    Edit

--- resources/views/pages/customers/profile/services.blade.php
+++ resources/views/pages/customers/profile/services.blade.php
@@ -12,21 +12,21 @@
         <tbody>
             @foreach ($services as $item)
             <tr>
-             <td><img src="{{ asset('public/uploads/'.$item->image) }}" width="50" height="50" alt=""></td>
-             <td>{{ $item->title }}</td>
-             <td>{{ $item->title }}</td>
-             <td>{{ date('Y-m-d', strtotime($item->created_at)); }}</td>
-             <td>
-                <a href="{{ route('services.edit', $item->id) }}" class="btn btn-info"><i class="fa fa-edit"></i></a>
-                <form style="display: inline"
-                    action="{{ route('services.destroy', $item->id) }}"
-                    method="post">
-                    @method('delete') @csrf
-                    <button type="submit" class="btn btn-danger delete-confirm"><i
-                            class="fa fa-trash"></i></button>
-                </form>
-            </td>
-            </tr>
+                <td><img src="{{ asset('public/uploads/'.$item->image) }}" width="50" height="50" alt=""></td>
+                <td>{{ $item->title }}</td>
+                <th><a href="{{ route('marketer.show',$item->user->id) }}">{{ $item->user->name }}</a></th>
+                <td>{{ date('Y-m-d', strtotime($item->created_at)); }}</td>
+                <td>
+                   <a href="{{ route('services.edit', $item->id) }}" class="btn btn-info"><i class="fa fa-edit"></i></a>
+                   <form style="display: inline"
+                       action="{{ route('services.destroy', $item->id) }}"
+                       method="post">
+                       @method('delete') @csrf
+                       <button type="submit" class="btn btn-danger delete-confirm"><i
+                               class="fa fa-trash"></i></button>
+                   </form>
+               </td>
+               </tr>
                 
             @endforeach
         </tfoot>
