dd51c38046c5b5af4ff7a0a0aa69a862a2f7ec86
islamshu12 <islamshu12@gmail.com>
2022-10-22 13:05:56 +0300

    edit

--- resources/views/pages/marketers/profile/chat.blade.php
+++ resources/views/pages/marketers/profile/chat.blade.php
@@ -224,77 +224,14 @@
                                         <div class="d-flex justify-content-center flex-column me-3">
                                             <a href="#" class="fs-4 fw-bold text-gray-900 text-hover-primary me-1 mb-2 lh-1">{{ $sender->name }}</a>
                                             <!--begin::Info-->
-                                            <div class="mb-0 lh-1">
-                                                <span class="badge badge-success badge-circle w-10px h-10px me-1"></span>
-                                                <span class="fs-7 fw-semibold text-muted">Active</span>
-                                            </div>
+                                            
                                             <!--end::Info-->
                                         </div>
                                         <!--end::User-->
                                     </div>
                                     <!--end::Title-->
                                     <!--begin::Card toolbar-->
-                                    <div class="card-toolbar">
-                                        <!--begin::Menu-->
-                                        <div class="me-n3">
-                                            <button class="btn btn-sm btn-icon btn-active-light-primary" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
-                                                <i class="bi bi-three-dots fs-2"></i>
-                                            </button>
-                                            <!--begin::Menu 3-->
-                                            <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-semibold w-200px py-3" data-kt-menu="true">
-                                                <!--begin::Heading-->
-                                                <div class="menu-item px-3">
-                                                    <div class="menu-content text-muted pb-2 px-3 fs-7 text-uppercase">Contacts</div>
-                                                </div>
-                                                <!--end::Heading-->
-                                                <!--begin::Menu item-->
-                                                <div class="menu-item px-3">
-                                                    <a href="#" class="menu-link px-3" data-bs-toggle="modal" data-bs-target="#kt_modal_users_search">Add Contact</a>
-                                                </div>
-                                                <!--end::Menu item-->
-                                                <!--begin::Menu item-->
-                                                <div class="menu-item px-3">
-                                                    <a href="#" class="menu-link flex-stack px-3" data-bs-toggle="modal" data-bs-target="#kt_modal_invite_friends">Invite Contacts 
-                                                    <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip" aria-label="Specify a contact email to send an invitation" data-bs-original-title="Specify a contact email to send an invitation" data-kt-initialized="1"></i></a>
-                                                </div>
-                                                <!--end::Menu item-->
-                                                <!--begin::Menu item-->
-                                                <div class="menu-item px-3" data-kt-menu-trigger="hover" data-kt-menu-placement="right-start">
-                                                    <a href="#" class="menu-link px-3">
-                                                        <span class="menu-title">Groups</span>
-                                                        <span class="menu-arrow"></span>
-                                                    </a>
-                                                    <!--begin::Menu sub-->
-                                                    <div class="menu-sub menu-sub-dropdown w-175px py-4">
-                                                        <!--begin::Menu item-->
-                                                        <div class="menu-item px-3">
-                                                            <a href="#" class="menu-link px-3" data-bs-toggle="tooltip" data-bs-original-title="Coming soon" data-kt-initialized="1">Create Group</a>
-                                                        </div>
-                                                        <!--end::Menu item-->
-                                                        <!--begin::Menu item-->
-                                                        <div class="menu-item px-3">
-                                                            <a href="#" class="menu-link px-3" data-bs-toggle="tooltip" data-bs-original-title="Coming soon" data-kt-initialized="1">Invite Members</a>
-                                                        </div>
-                                                        <!--end::Menu item-->
-                                                        <!--begin::Menu item-->
-                                                        <div class="menu-item px-3">
-                                                            <a href="#" class="menu-link px-3" data-bs-toggle="tooltip" data-bs-original-title="Coming soon" data-kt-initialized="1">Settings</a>
-                                                        </div>
-                                                        <!--end::Menu item-->
-                                                    </div>
-                                                    <!--end::Menu sub-->
-                                                </div>
-                                                <!--end::Menu item-->
-                                                <!--begin::Menu item-->
-                                                <div class="menu-item px-3 my-1">
-                                                    <a href="#" class="menu-link px-3" data-bs-toggle="tooltip" data-bs-original-title="Coming soon" data-kt-initialized="1">Settings</a>
-                                                </div>
-                                                <!--end::Menu item-->
-                                            </div>
-                                            <!--end::Menu 3-->
-                                        </div>
-                                        <!--end::Menu-->
-                                    </div>
+                                  
                                     <!--end::Card toolbar-->
                                 </div>
                                 <!--end::Card header-->
@@ -314,12 +251,12 @@
                                                 <div class="d-flex align-items-center mb-2">
                                                     <!--begin::Avatar-->
                                                     <div class="symbol symbol-35px symbol-circle">
-                                                        <img alt="Pic" src="/metronic8/demo1/assets/media/avatars/300-25.jpg">
+                                                        <img alt="Pic" src=" {{ asset('public/uploads/' . $item->image)  }}">
                                                     </div>
                                                     <!--end::Avatar-->
                                                     <!--begin::Details-->
                                                     <div class="ms-3">
-                                                        <a href="#" class="fs-5 fw-bold text-gray-900 text-hover-primary me-1">Brian Cox</a>
+                                                        <a href="#" class="fs-5 fw-bold text-gray-900 text-hover-primary me-1">{{ $item->name }}</a>
                                                         <span class="text-muted fs-7 mb-1">2 mins</span>
                                                     </div>
                                                     <!--end::Details-->
