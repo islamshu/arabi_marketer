652c78fbb0b1c3ac7798e390848e5855737758c3
islamshu12 <islamshu12@gmail.com>
2022-10-22 13:24:18 +0300

    f

--- resources/views/pages/marketers/profile/chat.blade.php
+++ resources/views/pages/marketers/profile/chat.blade.php
@@ -302,9 +302,31 @@
                                        
 
                                     @foreach ($messages as $item)
+                                    @if ($item->receiver_id != $sender->id)
+                                        <div class="d-flex flex-column align-items-start">
+                                        <!--begin::User-->
+                                        <div class="d-flex align-items-center mb-2">
+                                            <!--begin::Avatar-->
+                                            <div class="symbol symbol-35px symbol-circle">
+                                                <img alt="Pic" src="/metronic8/demo1/assets/media/avatars/300-25.jpg">
+                                            </div>
+                                            <!--end::Avatar-->
+                                            <!--begin::Details-->
+                                            <div class="ms-3">
+                                                <a href="#" class="fs-5 fw-bold text-gray-900 text-hover-primary me-1">Brian Cox</a>
+                                                <span class="text-muted fs-7 mb-1">2 mins</span>
+                                            </div>
+                                            <!--end::Details-->
+                                        </div>
+                                        <!--end::User-->
+                                        <!--begin::Text-->
+                                        <div class="p-5 rounded bg-light-info text-dark fw-semibold mw-lg-400px text-start" data-kt-element="message-text">{{ $item->message }}</div>
+                                        <!--end::Text-->
+                                    </div>
+                                    @else
                                     <div class="d-flex justify-content-end mb-10">
                                         <!--begin::Wrapper-->
-                                        <div class="d-flex flex-column @if ($item->receiver_id != $sender->id) align-items-start @else align-items-end @endif">
+                                        <div class="d-flex flex-column align-items-end ">
                                             <!--begin::User-->
                                             <div class="d-flex align-items-center mb-2">
                                                 <!--begin::Details-->
@@ -326,6 +348,7 @@
                                         </div>
                                         <!--end::Wrapper-->
                                     </div>
+                                    @endif
                                     @endforeach
 
 
