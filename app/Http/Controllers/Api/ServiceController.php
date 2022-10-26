1ca8396fc0cf26b5ba7fba6c9253c2d2eece1079
islamshu12 <islamshu12@gmail.com>
2022-10-26 14:47:14 +0300

    e

--- app/Http/Controllers/Api/ServiceController.php
+++ app/Http/Controllers/Api/ServiceController.php
@@ -76,7 +76,6 @@ class ServiceController extends BaseController
                 $service->user_id = auth('api')->id();
                 $image_array = array();
                 $images = explode(',',$request->images);
-                return $images;
                 foreach ($images as $key => $image) {
 
                     array_push($image_array, $image->store('service'));
