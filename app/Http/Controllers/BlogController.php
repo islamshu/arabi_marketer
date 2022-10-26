f59ab328705ed1b5df3ddd261fa2813e1b8ca61d
islamshu12 <islamshu12@gmail.com>
2022-10-26 11:30:15 +0300

    d

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -30,7 +30,7 @@ class BlogController extends Controller
         ->telegram()
         ->whatsapp()        
         ->reddit();
-        dd($shareComponent);
+        dd($shareComponent->generatedUrls);
         return view('pages.blogs.index')
         ->with('blogs',Blog::orderby('id','desc')->get())
         ->with('bending_blog',Blog::where('status',0)->orderby('id','desc')->get())
