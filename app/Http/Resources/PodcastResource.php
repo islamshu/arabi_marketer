e4192c561831f0eb1e8865c67bb7107dce4b105c
islamshu12 <islamshu12@gmail.com>
2022-10-31 16:49:46 +0200

    a

--- app/Http/Resources/PodcastResource.php
+++ app/Http/Resources/PodcastResource.php
@@ -38,7 +38,7 @@ class PodcastResource extends JsonResource
         $aa = array();
         $i =0;
         foreach($flux->channel->item as $flu){
-            $aa[$i]['title']=$flu->title;
+            $aa[$i]['title']=$flu->title->first();
             $aa[$i]['link']=utf8_decode((string)$flu->enclosure['url']);
             $i++;
         }
