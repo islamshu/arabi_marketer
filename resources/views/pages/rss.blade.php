2d42f3a09f2de59ac5dd885069c5439230682b24
islamshu12 <islamshu12@gmail.com>
2022-10-31 13:35:45 +0200

    a

--- resources/views/pages/rss.blade.php
+++ resources/views/pages/rss.blade.php
@@ -43,6 +43,7 @@ const play_btn{{ $i }} = document.querySelector('#play-btn{{ $i }}');
 //audio file
 
 let linkk{{ $i }} = document.getElementById("linkk{{ $i }}").getAttribute("href"); 
+alert(linkk0)
 let sound{{ $i }} = new Audio(linkk{{ $i }});
 //play event
 play_btn{{ $i }}.addEventListener( 'click' , play );
