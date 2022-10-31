6dd5e986bf0b99644f9595ccbcb6ebd9fa65d85e
islamshu12 <islamshu12@gmail.com>
2022-10-31 13:41:51 +0200

    s

--- resources/views/pages/rss.blade.php
+++ resources/views/pages/rss.blade.php
@@ -1,13 +1,10 @@
 @extends('layout.main')
 
 <x-base-layout>
-    
 
 @foreach ($flux->channel->item as $flu)
     <article class="entry-item">
-
-        {{-- <source src="https://feeds.soundcloud.com/stream/1371689950-ghandourpodcast-m3vyfsx9vxtf.mp3" type="audio/ogg">
-        <source src="" type="audio/mpeg"> --}}
+        <source src="{{utf8_decode((string)$flu->enclosure['url'])}}" type="audio/mpeg">
 
         <div class="entry-content">
             <a href="{{ $flu->item->link }}">{{ $flu->title }}</a>
