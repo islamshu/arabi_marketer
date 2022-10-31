980cfd2f5e5585b899bdfbecec9ea6ee86017bbd
islamshu12 <islamshu12@gmail.com>
2022-10-31 13:13:01 +0200

    a

--- resources/views/pages/rss.blade.php
+++ resources/views/pages/rss.blade.php
@@ -4,7 +4,9 @@
 
 @foreach ($flux->channel->item as $flu)
     <article class="entry-item">
-        <source src="{{utf8_decode((string)$flu->enclosure['url'])}}" type="audio/mpeg">
+
+        {{dd(utf8_decode((string)$flu->enclosure['url']))}}
+        <source src="" type="audio/mpeg">
 
         <div class="entry-content">
             <a href="{{ $flu->item->link }}">{{ $flu->title }}</a>
