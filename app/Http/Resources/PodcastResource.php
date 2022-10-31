003b061f34f9e8b152c763ad3bae84d4e0b89fae
islamshu12 <islamshu12@gmail.com>
2022-10-31 14:35:37 +0200

    a

--- app/Http/Resources/PodcastResource.php
+++ app/Http/Resources/PodcastResource.php
@@ -34,12 +34,8 @@ class PodcastResource extends JsonResource
     function get_item($data){
         $url = $data->sound_url;
         $content = file_get_contents($url);
-        $xmlObject = simplexml_load_string($content);
-                   
-        $json = json_encode($xmlObject);
-        $phpArray = json_decode($json, true); 
+        
         $flux = new SimpleXMLElement($content);
-        dd($phpArray);
         $aa = array();
         $i =0;
         foreach($flux->channel->item as $flu){
