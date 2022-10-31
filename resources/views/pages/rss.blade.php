ac9a7d0a26ef6110282c3a943243bf8149ad05e4
islamshu12 <islamshu12@gmail.com>
2022-10-31 13:42:44 +0200

    a

--- resources/views/pages/rss.blade.php
+++ resources/views/pages/rss.blade.php
@@ -1,16 +1,62 @@
 @extends('layout.main')
+<style>
+    body {
+	margin: 0;
+	padding: 0;
+}
 
+p {
+	padding: 10px;
+}
+
+.btns {
+  margin: 10px 6px;
+  height: 70px;
+  width: 70px;
+  color: #FFF;
+  font-size: 16px;
+  background-color: #696969;
+  border: none;
+  border-radius: 50%;
+  outline: none;
+  cursor: pointer;
+  transition-duration: 0.5s;
+}
+
+.btns:hover {
+  background-color: #000;
+}
+</style>
 <x-base-layout>
 
+@php
+    $i =0;
+@endphp
 @foreach ($flux->channel->item as $flu)
-    <article class="entry-item">
-        <source src="{{utf8_decode((string)$flu->enclosure['url'])}}" type="audio/mpeg">
+<button class="btns" id="play-btn{{ $i }}">play</button>
+<p><a href="{{utf8_decode((string)$flu->enclosure['url'])}}" id="linkk{{ $i }}" target="_blank" rel="noopener">{{ $flu->title }}</a></p>
 
-        <div class="entry-content">
-            <a href="{{ $flu->item->link }}">{{ $flu->title }}</a>
-            {{ $flu->description }}
-        </div>
-    </article>
+<script>
+    //play button
+const play_btn{{ $i }} = document.querySelector('#play-btn{{ $i }}');
+
+//audio file
+
+let linkk{{ $i }} = document.getElementById("linkk{{ $i }}").getAttribute("href"); 
+console.log("{{ $flu->item->link }}")
+let sound{{ $i }} = new Audio(linkk{{ $i }});
+//play event
+play_btn{{ $i }}.addEventListener( 'click' , play );
+
+function play(){
+	sound{{ $i }}.play();
+}
+</script>
+<hr>
+  @php
+      $i++;
+  @endphp
 @endforeach
 </x-base-layout>
+
 
