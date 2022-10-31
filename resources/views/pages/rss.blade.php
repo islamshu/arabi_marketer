d7ef71d490a47aa1cd76ce457eb162ae28da5cff
islamshu12 <islamshu12@gmail.com>
2022-10-31 13:25:35 +0200

    a

--- resources/views/pages/rss.blade.php
+++ resources/views/pages/rss.blade.php
@@ -1,18 +1,54 @@
 @extends('layout.main')
+<style>
+    body {
+	margin: 0;
+	padding: 0;
+}
 
+p {
+	padding: 10px;
+}
+
+.btns {
+  margin: 10px 6px;
+  height: 70px;
+  width: 70px;
+  color: #FFF;
+  font-size: 16px;
+  background-color: #696969;
+  border: none;
+  border-radius: 50%;
+  outline: none;
+  cursor: pointer;
+  transition-duration: 0.5s;
+}
+
+.btns:hover {
+  background-color: #000;
+}
+</style>
 <x-base-layout>
 
+
 @foreach ($flux->channel->item as $flu)
-    <article class="entry-item">
-
-        {{-- <source src="https://feeds.soundcloud.com/stream/1371689950-ghandourpodcast-m3vyfsx9vxtf.mp3" type="audio/ogg">
-        <source src="" type="audio/mpeg"> --}}
-
-        <div class="entry-content">
-            <a href="{{ $flu->item->link }}">{{ $flu->title }}</a>
-            {{ $flu->description }}
-        </div>
-    </article>
+<button class="btns" id="play-btn">play</button>
+<hr>
+<p><a href="{{ $flu->item->link }}" target="_blank" rel="noopener">{{ $flu->title }}</a></p>
+  
 @endforeach
 </x-base-layout>
+<script>
+    //play button
+const play_btn = document.querySelector('#play-btn');
+
+//audio file
+let sound = new Audio("https://raw.githubusercontent.com/Yousuke777/sound/main/kansei.mp3");
+
+//play event
+play_btn.addEventListener( 'click' , play );
+
+function play(){
+	sound.play();
+}
+</script>
 
