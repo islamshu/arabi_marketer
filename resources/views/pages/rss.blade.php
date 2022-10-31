79ebab8049e511b06a60dc41a55a2158ef2ac24d
islamshu12 <islamshu12@gmail.com>
2022-10-31 13:04:55 +0200

    d

--- resources/views/pages/rss.blade.php
+++ resources/views/pages/rss.blade.php
@@ -2,14 +2,7 @@
 
 <x-base-layout>
 
-@foreach ($flux->channel->item as $flu)
-    <article class="entry-item">
-        <img src="{{utf8_decode((string)$flu->enclosure['url'])}}" alt="">
-        <div class="entry-content">
-            <a href="{{ $flu->item->link }}">{{ $flu->title }}</a>
-            {{ $flu->description }}
-        </div>
-    </article>
-@endforeach
+    <enclosure url="https://www.w3schools.com/xml/rss.mp3"
+    length="5000" type="audio/mpeg" />
 </x-base-layout>
 
