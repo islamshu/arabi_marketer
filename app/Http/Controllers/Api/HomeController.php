b5bf74e39ca6eee9a7a3a1c99cbe88e79be75463
islamshu12 <islamshu12@gmail.com>
2022-10-31 12:51:25 +0200

    a

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -93,7 +93,7 @@ class HomeController extends BaseController
     public function rss(){
         $f = FeedReader::read('https://feeds.soundcloud.com/users/soundcloud:users:186745249/sounds.rss');
 
-        dd($f,$f->get_content(),$f->get_title());
+        dd($f,$f->get_title());
         // echo $f->get_items()[0]->get_title();
         // echo $f->get_items()->get_content();
     }
