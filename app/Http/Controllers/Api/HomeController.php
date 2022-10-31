7c40b353786d191c749942432defe7fc5bd544e1
islamshu12 <islamshu12@gmail.com>
2022-10-31 12:55:30 +0200

    a

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -103,6 +103,7 @@ class HomeController extends BaseController
 {
     $content = file_get_contents('https://feeds.soundcloud.com/users/soundcloud:users:186745249/sounds.rss');
     $flux = new SimpleXMLElement($content);
+    dd($flux);
 
     return View::make('pages.rss', compact('flux'));
 }
