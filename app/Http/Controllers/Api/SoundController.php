e908ac103100a253bacd8e945b74b419afd23044
islamshu12 <islamshu12@gmail.com>
2022-11-01 12:53:30 +0200

    edit

---
+++ app/Http/Controllers/Api/SoundController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Validator;
+use App\Http\Controllers\Api\BaseController;
+use App\Models\Sound;
+
+class SoundController extends BaseController
+{
+    public function store(Request $request){
+        $validation = Validator::make($request->all(), [
+            'sound'=>'mimes:mp3',
+        ]);
+        $sound = new Sound();
+        $sound->sound = $request->sound->store('sound');
+        $sound->user_id = auth('api')->id();
+        $sound->save();
+        return $this->sendResponse('success','success');
+    }
+}
