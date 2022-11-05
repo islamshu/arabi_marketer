7848ed2f8e4141fe402152dc2d68a582549533f3
islamshu12 <islamshu12@gmail.com>
2022-11-05 15:04:43 +0200

    d

--- resources/views/pages/podcasts/new_index.blade.php
+++ resources/views/pages/podcasts/new_index.blade.php
@@ -83,8 +83,8 @@
 
             
             <div class="tab-content" id="myTabContent">
-                <div class="modal bg-white fade" tabindex="-1" id="kt_modal_2">
-                    <div class="modal-dialog modal-fullscreen">
+                <div class="modal fade" tabindex="-1" id="kt_modal_2">
+                    <div class="modal-dialog modal-lg">
                         <div class="modal-content shadow-none">
                             <div class="modal-header">
                                 <h3 class="modal-title">Modal title</h3>
