5aa80cf399041787eb68d91cc078b0ea0b23909b
islamshu12 <islamshu12@gmail.com>
2022-11-06 22:15:16 +0200

    ed

--- resources/views/pages/tickets/show.blade.php
+++ resources/views/pages/tickets/show.blade.php
@@ -89,7 +89,7 @@
                             </div>
                           </div>
                          </div>
-                         <div class="form-group col-md-6">
+                         <div class="form-group col-md-8">
 
                             <div class="card">
                              
@@ -98,7 +98,7 @@
                                 @csrf
                                 <input type="hidden" class="form-control" name="ticket_id" value="{{ $ticket->id }}" >
                                 <textarea name="body" class="form-control" id="" cols="30" rows="4"></textarea>
-                                <div class="col-md-3">
+                                <div class="col-md-3 mt-3">
                                     <input type="submit" value="send" class="form-control btn btn-info">
 
                                 </div>
