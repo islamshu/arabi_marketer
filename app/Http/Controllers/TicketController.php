04175dd19a9639baa1816704b8d49c38af1aa00f
islamshu12 <islamshu12@gmail.com>
2022-11-06 22:56:51 +0200

    edit

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -29,7 +29,7 @@ class TicketController extends Controller
         $ticket = Ticket::find($request->ticket_id);
         $ticket->status = 2;
         $ticket->save();
-        dd($ticket);
+        // dd($ticket);
         Alert::success('Success', 'Replay successfully');
         return redirect()->back();
     }
