bc444e8ac1a3d09912afd7ea25e7cff019ffc9cf
islamshu12 <islamshu12@gmail.com>
2022-11-06 13:16:29 +0200

    edit

--- app/Models/OwenPodcast.php
+++ app/Models/OwenPodcast.php
@@ -9,4 +9,13 @@ class OwenPodcast extends Model
 {
     use HasFactory;
     protected $guarded=[];
+    /**
+     * Get all of the comments for the OwenPodcast
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function sounds()
+    {
+        return $this->hasMany(Sound::class, 'podcast_id');
+    }
 }
