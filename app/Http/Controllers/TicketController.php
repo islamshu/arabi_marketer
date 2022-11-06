0ea4860e30b23d33dd77af98cec57c1a03dfb8ea
islamshu12 <islamshu12@gmail.com>
2022-11-06 22:36:16 +0200

    edi

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -9,28 +9,34 @@ use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
-    public function index(){
-        return view('pages.tickets.index')->with('tickets',Ticket::orderBy('id','desc')->get());
+    public function index()
+    {
+        return view('pages.tickets.index')->with('tickets', Ticket::orderBy('id', 'desc')->get());
     }
-    public function destroy($id){
+    public function destroy($id)
+    {
         Ticket::find($id)->delete();
         Alert::success('Success', 'Deleted successfully');
         return redirect()->back();
     }
-    public function send_replay(Request $request){
-        $replay = new TicketReply() ;
+    public function send_replay(Request $request)
+    {
+        $replay = new TicketReply();
         $replay->body = $request->body;
         $replay->ticket_id = $request->ticket_id;
         $replay->user_id = auth()->id();
-
         $replay->save();
+        $ticket = Ticket::find($request->ticket_id);
+        $ticket->status = 2;
+        $ticket->save();
         Alert::success('Success', 'Replay successfully');
         return redirect()->back();
     }
-    public function show($id){
-       $ticket =  Ticket::find($id) ;
-       $ticket->status =1;
-       $ticket->save();
-       return view('pages.tickets.show')->with('ticket',$ticket);
+    public function show($id)
+    {
+        $ticket =  Ticket::find($id);
+        $ticket->status = 1;
+        $ticket->save();
+        return view('pages.tickets.show')->with('ticket', $ticket);
     }
 }
