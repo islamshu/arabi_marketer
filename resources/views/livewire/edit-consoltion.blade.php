c5dc0798ec7e7d4158ca828f172752c48f2ffad4
islamshu12 <islamshu12@gmail.com>
2022-11-07 14:31:18 +0200

    f

--- resources/views/livewire/edit-consoltion.blade.php
+++ resources/views/livewire/edit-consoltion.blade.php
@@ -25,7 +25,7 @@
                 <label for="title" class="form-label">عنوان الاستشارة</label>
                 {{ $title .' '.$days }}
                 @foreach ($days as $item)
-                    {{ $item }} <br>
+                    {{ $item->day }} <br>
                 @endforeach
                 <input type="text" wire:model="title" 
                     class="form-control {{ $errors->first('day') ? 'is-invalid' : '' }}" id="title"
