b8fb23c391a2d362b7f403f5da58e5246aecc2b6
islamshu12 <islamshu12@gmail.com>
2022-11-07 15:21:36 +0200

    d

--- app/Http/Livewire/EditConsoltion.php
+++ app/Http/Livewire/EditConsoltion.php
@@ -81,8 +81,8 @@ class EditConsoltion extends Component
     }
     public function remove($i)
     {   
-        // dd($i);
-        unset($i);
+        dd($this->i);
+        unset($this->i);
     }
     public function remove_key($i)
     {
