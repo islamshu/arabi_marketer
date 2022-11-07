3d080bbc0627c1eb160d4d33ed7a62422c380f07
islamshu12 <islamshu12@gmail.com>
2022-11-07 14:52:12 +0200

    d

--- resources/views/livewire/edit-consoltion.blade.php
+++ resources/views/livewire/edit-consoltion.blade.php
@@ -177,7 +177,7 @@
             <div class="mb-3 row">
                 <div class="col-md-4">
                     <label for="phone" class="form-label"> اليوم </label>
-                    <select name="day[]" id="day" wire:model="day.[{{ $key }}]"
+                    <select name="day[]" id="day" wire:model="day_select.0"
                         class="form-control {{ $errors->first('day.0') ? 'is-invalid' : '' }}">
                         <option value="" selected></option>
                         <option value="Monday">الاثنين</option>
