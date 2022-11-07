5f18dacd0e882b8d2bc9c56e287d6a009bdf7336
islamshu12 <islamshu12@gmail.com>
2022-11-07 13:48:53 +0200

    edit

--- resources/views/livewire/edit-consoltion.blade.php
+++ resources/views/livewire/edit-consoltion.blade.php
@@ -269,6 +269,105 @@
                 </div>
             @endforeach --}}
             <div class="mb-3 row">
+                <div class="col-md-4">
+                    <label for="phone" class="form-label"> اليوم </label>
+                    <select name="day[]" id="day" wire:model="day.0"
+                        class="form-control {{ $errors->first('day.0') ? 'is-invalid' : '' }}">
+                        <option value="" selected></option>
+                        <option value="Monday">الاثنين</option>
+                        <option value="Tuesday">الثلاثا</option>
+                        <option value="Wednesday">الاربعا</option>
+                        <option value="Thursday">الخميس</option>
+                        <option value="Friday">الجمعة</option>
+                        <option value="Saturday">السبت</option>
+                        <option value="Sunday">الأحد</option>
+
+                    </select>
+                    @error('day.0')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+                <div class="col-md-3">
+
+                    <label for="from" class="form-label"> من</label>
+                    <input type="time" wire:model="from.0"
+                        class="form-control {{ $errors->first('from.0') ? 'is-invalid' : '' }}" id="from">
+                    @error('from.0')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+                <div class="col-md-3">
+
+                    <label for="to" class="form-label"> الى</label>
+                    <input type="time" wire:model="to.0"
+                        class="form-control {{ $errors->first('to') ? 'is-invalid' : '' }}" id="to">
+                    @error('to.0')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+            </div>
+            <button class="btn text-white btn-info btn-sm" wire:click.prevent="add({{ $i }})">اضف
+                اوقات</button>
+            @foreach ($inputs as $key => $value)
+                <div class="mb-3 row">
+                    <div class="col-md-4">
+                        <label for="phone" class="form-label"> اليوم </label>
+                        <select name="day[]" id="day" wire:model="day.{{ $value }}"
+                            class="form-control {{ $errors->first('day.' . $value) ? 'is-invalid' : '' }}">
+                            <option value="" selected></option>
+                            <option value="Monday">الاثنين</option>
+                            <option value="Tuesday">الثلاثا</option>
+                            <option value="Wednesday">الاربعا</option>
+                            <option value="Thursday">الخميس</option>
+                            <option value="Friday">الجمعة</option>
+                            <option value="Saturday">السبت</option>
+                            <option value="Sunday">الأحد</option>
+
+                        </select>
+                        @error('day.' . $value)
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="col-md-3">
+
+                        <label for="from" class="form-label"> من</label>
+                        <input type="time" wire:model="from.{{ $value }}"
+                            class="form-control {{ $errors->first('from.' . $value) ? 'is-invalid' : '' }}"
+                            id="from">
+                        @error('from.' . $value)
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="col-md-3">
+
+                        <label for="to" class="form-label"> الى</label>
+                        <input type="time" wire:model="to.{{ $value }}"
+                            class="form-control {{ $errors->first('to.' . $value) ? 'is-invalid' : '' }}"
+                            id="to">
+                        @error('to.' . $value)
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="col-md-2">
+                        <button class="btn btn-danger btn-sm" style="margin-top:13%"
+                            wire:click.prevent="remove({{ $key }})">حذف</button>
+                    </div>
+                </div>
+            @endforeach
+            <div class="mb-3 row">
                 <div class="col-md-6">
                     <label for="price" class="form-label"> سعر الاستشارة   </label>
                     <input type="range" min="10" max="500" wire:model="price"
