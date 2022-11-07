d09e22a3399849c90c5d1b09df5fe69e4a830218
islamshu12 <islamshu12@gmail.com>
2022-11-07 15:10:54 +0200

    f

--- app/Http/Livewire/EditConsoltion.php
+++ app/Http/Livewire/EditConsoltion.php
@@ -166,6 +166,7 @@ class EditConsoltion extends Component
             'payment_id'=>$this->payment,
             'user_id'=>$this->user,
         ]);
+        dd($this->day_select,$this->form_select,$to_select);
         ConsutingDate::where('consulte_id',$cond->id)->delete();
         foreach ($this->day_select as $key => $value) {
 
