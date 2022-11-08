05fa3ab1f6fe33ff16f96e656d4780f96d02317e
islamshu12 <islamshu12@gmail.com>
2022-11-08 15:25:16 +0200

    d

--- resources/views/pages/blogs/index.blade.php
+++ resources/views/pages/blogs/index.blade.php
@@ -130,15 +130,18 @@
         });
    </script>
    <script>
-    ClassicEditor
-            .create( document.querySelector( '.editor' ) )
-            .then( editor => {
-                    console.log( editor );
-            } )
-            .catch( error => {
-                    console.error( error );
-            } );
-            var input1 = document.querySelector("#kt_tagify_3");
-            new Tagify(input1);
+    // ClassicEditor
+    //         .create( document.querySelector( '.editor' ) )
+    //         .then( editor => {
+    //                 console.log( editor );
+    //         } )
+    //         .catch( error => {
+    //                 console.error( error );
+    //         } );
+    //         var input1 = document.querySelector("#kt_tagify_3");
+    //         new Tagify(input1);
+            CKEDITOR.replace( 'editor' );
+
 </script>
+
 @endsection
