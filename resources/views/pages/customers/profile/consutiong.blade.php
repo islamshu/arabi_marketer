8eb6025bde31230bd7dc9b4ce994dd814c86f646
islamshu12 <islamshu12@gmail.com>
2022-11-09 13:53:08 +0200

    edit

--- resources/views/pages/customers/profile/consutiong.blade.php
+++ resources/views/pages/customers/profile/consutiong.blade.php
@@ -12,8 +12,8 @@
     <tbody>
         @foreach ($consls as $item)
         <tr>
-         <td>{{ $consult->title }}</td>
-         <th><a href="{{ route('marketer.show',$item->consultion->user->id) }}">{{ $item->consultion->user->name }}</a></th>
+         <td>{{ $item->consult->title }}</td>
+         <th><a href="{{ route('marketer.show',$item->consult->user->id) }}">{{ $item->consult->user->name }}</a></th>
          <th>{{ $item->price }}</th>
          <th>{{ date('Y-m-d', strtotime($item->created_at)) }}</th>
          <th><a target="_blank" href="{{ route('order_consulting.show',$item->order->id) }}"><i class="fa fa-eye"></i></a> </th>
