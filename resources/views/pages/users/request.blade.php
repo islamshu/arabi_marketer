f91290b59cc1189fd478bf2d3512fdde558f31de
islamshu12 <islamshu12@gmail.com>
2022-11-13 15:07:38 +0200

    d

--- resources/views/pages/users/request.blade.php
+++ resources/views/pages/users/request.blade.php
@@ -42,6 +42,7 @@
                                 </tr>
                             </thead>
                             <tbody class="text-gray-600 fw-semibold">
+                                {{ dd($requests) }}
                                 @foreach ($requests as $key=>$item)
                                     <td>{{ $key+1 }}</td>
                                     <td>{{ $item->user->name }}</td>
