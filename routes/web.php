6402414a8442539af4397db20fe7432bca5e2bf8
islamshu12 <islamshu12@gmail.com>
2022-11-13 15:05:29 +0200

    d

--- routes/web.php
+++ routes/web.php
@@ -89,7 +89,7 @@ Route::middleware('auth')->group(function () {
     Route::resource('tickets', TicketController::class);
     Route::post('send_replay',[TicketController::class,'send_replay'])->name('send_replay');
     Route::get('marketers',[ProfileController::class,'markters']);
-    Route::get('marketers_requests',[UserCon::class,'markters']);
+    Route::get('marketers_requests',[UsersController::class,'marketers_requests'])->name('marketers_requests');
 
     
     Route::get('customers',[ProfileController::class,'users']);
