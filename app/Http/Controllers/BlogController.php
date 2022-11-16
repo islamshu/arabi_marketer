1ecb34861f97118e0aa05dde86908a5d4d9fc50f
islamshu12 <islamshu12@gmail.com>
2022-11-16 15:51:13 +0200

    d

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -91,7 +91,8 @@ class BlogController extends Controller
 
                 // dd($request->keywords);
 
-                foreach (json_decode($request->tags) as $s) {
+                foreach (($request->tags) as $s) {
+                    dd($s);
                     $tag = new Tag();
                     $tag->title =  $s;
                     $tag->blog_id = $service->id;
