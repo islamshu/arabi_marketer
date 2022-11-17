c9c2a310dc499613ed8e00b01a531b8fdae0f359
islamshu12 <islamshu12@gmail.com>
2022-11-17 14:03:41 +0200

    d

--- resources/views/pages/blogs/_create.blade.php
+++ resources/views/pages/blogs/_create.blade.php
@@ -96,13 +96,8 @@
 </form>
 
 
-<div
-              class="modal fade"
-              id="exampleModal"
-              tabindex="-1"
-              aria-labelledby="exampleModalLabel"
-              aria-hidden="true"
-            >
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"
+    style="direction: rtl">
               <div class="modal-dialog">
                 <div class="modal-content">
                   <div class="modal-header">
