7236e604e8696690df67caf2cdafa5f8cd2e9782
islamshu12 <islamshu12@gmail.com>
2022-11-17 12:28:19 +0200

    f

--- resources/views/pages/blogs/all_image.blade.php
+++ resources/views/pages/blogs/all_image.blade.php
@@ -3,7 +3,7 @@
 <div class="item{{ $item->id }} blogsimage" 
 >
 <div class="img-box">
-  <img src="{{ asset('public/uploads/'.$item->image)  }}" alt="" />
+  <img src="{{ asset('public/uploads/'.$item->image)  }}" width="150" height="150" alt="" />
 </div>
 </div>
 @endforeach
