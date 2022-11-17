3b33416c24789d5fa7ee2c9fc802c6e4a96268e4
islamshu12 <islamshu12@gmail.com>
2022-11-17 12:50:16 +0200

    d

--- resources/views/pages/blogs/all_image.blade.php
+++ resources/views/pages/blogs/all_image.blade.php
@@ -1,5 +1,5 @@
 @foreach ($images as $item)
-    <div class="col-md-3 blogsimage" >
+    <div class="col-md-3 " >
         <div class="item{{ $item->id }} item ">
             <div class="img-box">
                 <img src="{{ asset('public/uploads/' . $item->image) }}" width="150" height="150" alt="" />
