3f9aed1400489d099dfaa0877c9a696423f6b1c9
islamshu12 <islamshu12@gmail.com>
2022-11-19 12:11:46 +0200

    edit

--- routes/api.php
+++ routes/api.php
@@ -44,7 +44,7 @@ use Illuminate\Support\Facades\Route;
 // Sample API route
 Route::get('/profits', [SampleDataController::class, 'profits'])->name('profits');
 Route::get('/testapi', [HomeController::class, 'testapi'])->name('testapi');
-Route::get('/change_mention', [HomeController::class, 'change_mention'])->name('change_mention');
+Route::get('/change_mention', [UserController::class, 'change_mention'])->name('change_mention');
 
 
 
