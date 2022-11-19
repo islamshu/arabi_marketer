04185d66b890a46bbf240de0e4141816ff46a0e0
islamshu12 <islamshu12@gmail.com>
2022-11-19 11:39:59 +0200

    edit

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -75,7 +75,7 @@ class HomeController extends BaseController
     public function testapi(Request $request){
         $mystring = 'islamshu12@gmail.com';
         $first = strtok($mystring, '@');
-        dd();
+        dd($first);
     }
     public function get_markter($id)
     {
