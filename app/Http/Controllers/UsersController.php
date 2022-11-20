8814d35c331ba45eb3e231271dcb93b0191c62b2
islamshu12 <islamshu12@gmail.com>
2022-11-20 11:20:30 +0200

    f

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -25,6 +25,7 @@ class UsersController extends Controller
     public function create()
     {
         $roles = Role::pluck('name','name')->all();
+        dd($roles);
        return view('pages.users.create')->with('roles');
     }
     
