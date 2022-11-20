6153dcefa291f6c8ade64d4079cde774d14f3e61
islamshu12 <islamshu12@gmail.com>
2022-11-20 15:07:03 +0200

    islam

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -32,7 +32,9 @@ class UsersController extends Controller
     public function store(Request $request){
         $check = User::where('email',$request->email)->where('type','staff')->first();
         if($check){
-            return redirect()->back()->with(['error'=>'البريد الاكتروني مستخدم من قبل']);
+            return redirect()->back()->with(['error'=>'البريد الاكتروني مستخدم من قبل']) 
+            ->withInput($request->all());
+
         }
        $request->validate([
         'name' => 'required',
