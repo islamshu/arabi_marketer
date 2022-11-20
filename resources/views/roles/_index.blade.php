3752d0b6f21ff466f21db9f7e2ef7135622f3c47
islamshu12 <islamshu12@gmail.com>
2022-11-20 11:04:15 +0200

    edit

--- resources/views/roles/_index.blade.php
+++ resources/views/roles/_index.blade.php
@@ -4,25 +4,25 @@
             <tr>
                 <th># </th>
                 <th>اسم الدور</th>
-     
+
                 <th>العمليات</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($roles as $key => $role)
-            <tr>
-            <td>{{ $key+1 }}</td>
-            <td>{{ $role->name }}</td>
-            <td>
-            <a class="btn btn-info" href="{{ route('roles.show',$role->id) }}"><i class="fa fa-eye"></i></a>
-            <a class="btn btn-primary" href="{{ route('roles.edit',$role->id) }}"><i class="fa fa-edit"></i></a>
-       
-            {!! Form::open(['method' => 'DELETE','route' => ['roles.destroy', $role->id],'style'=>'display:inline']) !!}
-            {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-            {!! Form::close() !!}
-            </td>
-            </tr>
+                <tr>
+                    <td>{{ $key + 1 }}</td>
+                    <td>{{ $role->name }}</td>
+                    <td>
+                        <a class="btn btn-primary" href="{{ route('roles.edit', $role->id) }}"><i
+                                class="fa fa-edit"></i></a>
+
+                        {!! Form::open(['method' => 'DELETE', 'route' => ['roles.destroy', $role->id], 'style' => 'display:inline']) !!}
+                        {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+                        {!! Form::close() !!}
+                    </td>
+                </tr>
             @endforeach
-        </tfoot>
+            </tfoot>
     </table>
 </div>
