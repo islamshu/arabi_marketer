d2c74f0d165f2be32d30c164d5048e8eb4b970e0
islamshu12 <islamshu12@gmail.com>
2022-11-21 11:40:23 +0200

    d

--- resources/views/pages/tools/_index.blade.php
+++ resources/views/pages/tools/_index.blade.php
@@ -10,7 +10,7 @@
         <tbody>
             @foreach ($tools as $item)
             <tr>
-             <td><img src="{{ asset('public/uploads/'.$item->image) }}" width="50" height="50" alt=""></td>
+             <td><img src="{{ URL::to('/') }}/public/uploads/{{ $item->image }}" width="50" height="50" alt=""></td>
              <td>{{ $item->title }}</td>
              <td>
                 <a href="{{ route('tools.edit', $item->id) }}" class="btn btn-info"><i class="fa fa-edit"></i></a>
