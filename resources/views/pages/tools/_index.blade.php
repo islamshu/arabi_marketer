6d7c9a7a9c4115e77843ab0112ddfd997d6ee504
islamshu12 <islamshu12@gmail.com>
2022-11-21 11:41:59 +0200

    edit

--- resources/views/pages/tools/_index.blade.php
+++ resources/views/pages/tools/_index.blade.php
@@ -4,13 +4,14 @@
             <tr>
                 <th>صورة الاداة</th>
                 <th>اسم الاداة</th>
+
                 <th>العمليات</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($tools as $item)
             <tr>
-             <td><img src="{{ URL::to('/') }}/public/uploads/{{ $item->image }}" width="50" height="50" alt=""></td>
+             <td><img src="{{ asset('public/uploads/'.$item->image) }}" width="50" height="50" alt=""></td>
              <td>{{ $item->title }}</td>
              <td>
                 <a href="{{ route('tools.edit', $item->id) }}" class="btn btn-info"><i class="fa fa-edit"></i></a>
