d47763c4abe3da998f2cfffaf27706e0fb2656fc
islamshu12 <islamshu12@gmail.com>
2022-11-22 13:02:41 +0200

    f

--- app/Http/Resources/NotificationResourse.php
+++ app/Http/Resources/NotificationResourse.php
@@ -14,6 +14,7 @@ class NotificationResourse extends JsonResource
      */
     public function toArray($request)
     {
+        return $this->data;
         return [
             'id'=>$this->id,
             'title'=>$this->data['title'],
