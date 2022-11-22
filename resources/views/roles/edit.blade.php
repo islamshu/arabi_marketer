ad96682e1d2aae0918e3934ad91ec5bc19a6e406
islamshu12 <islamshu12@gmail.com>
2022-11-22 14:37:39 +0200

    d

--- resources/views/roles/edit.blade.php
+++ resources/views/roles/edit.blade.php
@@ -62,9 +62,11 @@
                         </div>
                         <div class="col-xs-12 col-sm-12 col-md-12">
                             <div class="form-group">
+                                <br><br>
+
                                 <strong>الاذونات:</strong>
                                 <br />
-                                <br><br>
+                                <br>
                                 <div class="body">
                                     <div class="card-columns">
                                         <div class="row">
