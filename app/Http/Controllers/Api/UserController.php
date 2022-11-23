405540c88c73e7765002d2aae5c3387703dbfc8d
islamshu12 <islamshu12@gmail.com>
2022-11-23 16:20:47 +0200

    edit

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -123,7 +123,7 @@ class UserController extends BaseController
         $user->last_name = $request->last_name;
         $user->country_id = $request->country_id;
         $user->save();
-        $enc= encrypt(20);
+        $enc= encrypt($user->id);
         $url = route('send_email.verfy',$enc);
 
         Mail::to($request->email)->send(new VerifyEmail($url));
