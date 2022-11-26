838bc19a8c0958fb2df75d2d4fdf8c47dc1d59ff
islamshu12 <islamshu12@gmail.com>
2022-11-26 15:21:52 +0200

    edit

--- resources/views/pages/marketers/create.blade.php
+++ resources/views/pages/marketers/create.blade.php
@@ -6,7 +6,42 @@
     <title>Laravel Multistep Livewire Form Example</title>
     @livewireStyles
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap">
-    <link href="{{ asset('multiform.css') }}" rel="stylesheet" id="bootstrap">
+    {{-- <link href="{{ asset('multiform.css') }}" rel="stylesheet" id="bootstrap"> --}}
+    <style>
+        .display-none {
+    display: none;
+}
+.multi-wizard-step p {
+    margin-top: 12px;
+}
+.stepwizard-row {
+    display: table-row;
+}
+.stepwizard {
+    display: table;
+    position: relative;
+    width: 100%;
+}
+.multi-wizard-step button[disabled] {
+    filter: alpha(opacity=100) !important;
+    opacity: 1 !important;
+}
+.stepwizard-row:before {
+    top: 14px;
+    bottom: 0;
+    content: " ";
+    width: 100%;
+    height: 1px;
+    z-order: 0;
+    position: absolute;
+    background-color: #fefefe;
+}
+.multi-wizard-step {
+    text-align: center;
+    position: relative;
+    display: table-cell;
+}
+    </style>
 </head>
 <body class="mt-5">
     <div class="container">
