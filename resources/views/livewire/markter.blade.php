bc3545bb3b1d4e9f3255652a79d7ad3e355b784d
islamshu12 <islamshu12@gmail.com>
2022-11-26 16:34:20 +0200

    d

--- resources/views/livewire/markter.blade.php
+++ resources/views/livewire/markter.blade.php
@@ -125,7 +125,7 @@
                 <tr>
                     <td>domains:</td>
                     @foreach ($selection as $item)
-                    <td><strong>{{ App\Category::find($item)->title }}</strong></td>
+                    <td><strong>{{ App\Models\Category::find($item)->title }}</strong></td>
 
                     @endforeach
                 </tr>
