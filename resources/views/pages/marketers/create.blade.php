6cbfefbd7d317ddf0d4eaadd56cefd9767d99943
islamshu12 <islamshu12@gmail.com>
2022-11-26 15:23:53 +0200

    edit

--- resources/views/pages/marketers/create.blade.php
+++ resources/views/pages/marketers/create.blade.php
@@ -1,57 +1,130 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel Multistep Livewire Form Example</title>
-    @livewireStyles
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap">
-    {{-- <link href="{{ asset('multiform.css') }}" rel="stylesheet" id="bootstrap"> --}}
-    <style>
-        .display-none {
-    display: none;
-}
-.multi-wizard-step p {
-    margin-top: 12px;
-}
-.stepwizard-row {
-    display: table-row;
-}
-.stepwizard {
-    display: table;
-    position: relative;
-    width: 100%;
-}
-.multi-wizard-step button[disabled] {
-    filter: alpha(opacity=100) !important;
-    opacity: 1 !important;
-}
-.stepwizard-row:before {
-    top: 14px;
-    bottom: 0;
-    content: " ";
-    width: 100%;
-    height: 1px;
-    z-order: 0;
-    position: absolute;
-    background-color: #fefefe;
-}
-.multi-wizard-step {
-    text-align: center;
-    position: relative;
-    display: table-cell;
-}
-    </style>
-</head>
-<body class="mt-5">
-    <div class="container">
-        <div class="text-center">
-            Laravel Form Wizard Example
+@extends('layout.main')
+@section('style')
+<style>
+    .display-none {
+        display: none;
+    }
+
+    .multi-wizard-step p {
+        margin-top: 12px;
+    }
+
+    .stepwizard-row {
+        display: table-row;
+    }
+
+    .stepwizard {
+        display: table;
+        position: relative;
+        width: 100%;
+    }
+
+    .multi-wizard-step button[disabled] {
+        filter: alpha(opacity=100) !important;
+        opacity: 1 !important;
+    }
+
+    .stepwizard-row:before {
+        top: 14px;
+        bottom: 0;
+        content: " ";
+        width: 100%;
+        height: 1px;
+        z-order: 0;
+        position: absolute;
+        background-color: #fefefe;
+    }
+
+    .multi-wizard-step {
+        text-align: center;
+        position: relative;
+        display: table-cell;
+    }
+</style>
+@endsection
+
+<x-base-layout>
+
+    <div class="card card-flush">
+        <!--begin::Card body-->
+        <div class="card-body">
+            <!--begin:::Tabs-->
+            <ul class="nav nav-tabs nav-line-tabs nav-line-tabs-2x border-transparent fs-4 fw-semibold mb-15"
+                role="tablist">
+                <!--begin:::Tab item-->
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link text-active-primary pb-5 " href="/">
+                        <!--begin::Svg Icon | path: icons/duotune/general/gen001.svg-->
+                        <span class="svg-icon svg-icon-2 me-2">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    d="M11 2.375L2 9.575V20.575C2 21.175 2.4 21.575 3 21.575H9C9.6 21.575 10 21.175 10 20.575V14.575C10 13.975 10.4 13.575 11 13.575H13C13.6 13.575 14 13.975 14 14.575V20.575C14 21.175 14.4 21.575 15 21.575H21C21.6 21.575 22 21.175 22 20.575V9.575L13 2.375C12.4 1.875 11.6 1.875 11 2.375Z"
+                                    fill="currentColor"></path>
+                            </svg>
+                        </span>
+                        <!--end::Svg Icon-->الرئيسية
+                    </a>
+
+                </li>
+                <li class="nav-item" role="presentation">
+                    <a class="nav-link text-active-primary pb-5 active">
+                        <!--begin::Svg Icon | path: icons/duotune/general/gen001.svg-->
+                        <span class="svg-icon svg-icon-2 me-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none">
+                                <path opacity="0.3"
+                                    d="M19 22H5C4.4 22 4 21.6 4 21V3C4 2.4 4.4 2 5 2H14L20 8V21C20 21.6 19.6 22 19 22ZM12.5 18C12.5 17.4 12.6 17.5 12 17.5H8.5C7.9 17.5 8 17.4 8 18C8 18.6 7.9 18.5 8.5 18.5L12 18C12.6 18 12.5 18.6 12.5 18ZM16.5 13C16.5 12.4 16.6 12.5 16 12.5H8.5C7.9 12.5 8 12.4 8 13C8 13.6 7.9 13.5 8.5 13.5H15.5C16.1 13.5 16.5 13.6 16.5 13ZM12.5 8C12.5 7.4 12.6 7.5 12 7.5H8C7.4 7.5 7.5 7.4 7.5 8C7.5 8.6 7.4 8.5 8 8.5H12C12.6 8.5 12.5 8.6 12.5 8Z"
+                                    fill="currentColor"></path>
+                                <rect x="7" y="17" width="6" height="2" rx="1"
+                                    fill="currentColor"></rect>
+                                <rect x="7" y="12" width="10" height="2" rx="1"
+                                    fill="currentColor"></rect>
+                                <rect x="7" y="7" width="6" height="2" rx="1"
+                                    fill="currentColor"></rect>
+                                <path d="M15 8H20L14 2V7C14 7.6 14.4 8 15 8Z" fill="currentColor"></path>
+                            </svg>
+                        </span>
+                        <!--end::Svg Icon-->انشاء استشارة  
+                    </a>
+
+                </li>
+
+
+            </ul>
+            <!--end:::Tabs-->
+            <!--begin:::Tab content-->
+            <div class="tab-content" id="myTabContent">
+
+                <div class="tab-pane fade active show" id="kt_ecommerce_settings_general" role="tabpanel">
+
+                    <!--begin::Form-->
+                    <livewire:markter>
+                        <!--end::Form-->
+                </div>
+
+            </div>
+            <!--end:::Tab content-->
         </div>
-        <livewire:markter />
+        <!--end::Card body-->
     </div>
-</body>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-@livewireScripts
-</html>
+
+</x-base-layout>
+@section('scripts')
+<script>
+    $( document ).ready(function() {
+  // Handler for .ready() called.
+
+    $( "#time_duration" ).change(function() {
+        if($(this).val() == 0){
+            
+            $("#hour_and_mins").show();
+
+        }else{
+            $("#hour_and_mins").hide();
+        }
+});
+});
+</script>
+
+@endsection
