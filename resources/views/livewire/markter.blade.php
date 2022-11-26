9776fb49ba15136d4b05dfdefa15cc9ab98d09b2
islamshu12 <islamshu12@gmail.com>
2022-11-26 15:43:54 +0200

    edit

--- resources/views/livewire/markter.blade.php
+++ resources/views/livewire/markter.blade.php
@@ -61,7 +61,9 @@
             <h3> Step 2</h3>
             <div class="form-group">
                 <label for="description">Team Status</label><br />
-                <select  wire:model="selection" name="p" id="p" class="form-control" multiple>
+                    <select class="form-select form-control form-select-solid " wire:model="selection" multiple
+                    required data-control="select2" data-close-on-select="false" data-placeholder="اختر"
+                    data-allow-clear="true">
                     @foreach($categoires as $perm)
                         <option value={{$perm->id}}>{{ $perm->title }}</option>
                     @endforeach
