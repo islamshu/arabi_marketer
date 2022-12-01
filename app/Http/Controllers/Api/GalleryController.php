5cc22a285e1522b46d0bfe8f7f95e24e965a8cbf
islamshu12 <islamshu12@gmail.com>
2022-12-01 14:11:12 +0200

    d

--- app/Http/Controllers/Api/GalleryController.php
+++ app/Http/Controllers/Api/GalleryController.php
@@ -20,7 +20,7 @@ class GalleryController extends BaseController
         foreach(BlogImage::get() as $blog){
 
             $input['imagename'] = str_replace('blog/','',$blog->image);
-            $rand = $input['imagename'].rand(111111111,999999999);
+            $rand = rand(111111111,999999999).$input['imagename'];
             $destinationPath = public_path('uploads/blog/').$input['imagename'];
             $dess = public_path('uploads/blog');
             // dd($destinationPath);
