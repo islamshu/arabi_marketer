0f7dc4bb1e16f764b16838ed85a29e9602628ac2
islamshu12 <islamshu12@gmail.com>
2022-12-20 14:55:26 +0200

    edit

--- resources/views/pages/category_service/_index.blade.php
+++ resources/views/pages/category_service/_index.blade.php
@@ -1,4 +1,5 @@
 <div class="card-body">
+    
     <table id="kt_datatable_example_2" class="table align-middle table-row-dashed fs-6 gy-5">
         <thead>
             <tr class="text-start text-gray-400 fw-bold fs-7 text-uppercase gs-0">
