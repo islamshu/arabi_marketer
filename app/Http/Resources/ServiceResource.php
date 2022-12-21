a4d659a896c03a5dc91310a656ea0e3457d66af9
islamshu12 <islamshu12@gmail.com>
2022-12-21 12:49:16 +0200

    edit

--- app/Http/Resources/ServiceResource.php
+++ app/Http/Resources/ServiceResource.php
@@ -28,6 +28,7 @@ class ServiceResource extends JsonResource
             'specialties'=>$this->get_specialties($this),
             'files'=>$this->get_files($this),
             'number_of_view'=>$this->viewer,
+            'extra'=>ExtraServics::collection(ExtraServics::where('service_id',$this->id)->get()),
             'comment_number'=>$this->comments->where('status',1)->count(),
             'comments'=>CommentResourse::collection($this->comments->where('status',1)),
             'url_to_this_service'=>route('single_service',$this->id),
