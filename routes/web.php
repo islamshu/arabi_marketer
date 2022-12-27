28ad951f2383dad03f161c9a43249062a6e8e75b
islamshu12 <islamshu12@gmail.com>
2022-12-27 11:27:58 +0200

    edit

--- routes/web.php
+++ routes/web.php
@@ -99,8 +99,8 @@ Route::middleware('auth')->group(function () {
 
     
     Route::get('customers',[ProfileController::class,'users']);
-    Route::get('markter_order',[ProfileController::class,'markter_order']);
-    Route::get('show_customer_markter/{id}',[ProfileController::class,'show_customer_markter']);
+    Route::get('markter_order',[ProfileController::class,'markter_order'])->name('markter_order');
+    Route::get('show_customer_markter/{id}',[ProfileController::class,'show_customer_markter'])->name('show_customer_markter');
 
     
     Route::get('customer/{id}',[ProfileController::class,'show_customer'])->name('customer.show');
