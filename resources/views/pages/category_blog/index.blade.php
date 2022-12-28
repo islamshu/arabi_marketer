e57cc3a53ff9a82572bcc17124398dd90bac3c15
islamshu12 <islamshu12@gmail.com>
2022-12-28 17:40:01 +0200

    edit

--- resources/views/pages/category_blog/index.blade.php
+++ resources/views/pages/category_blog/index.blade.php
@@ -213,6 +213,8 @@
         </div>
     </div>
 </div>
+@include('sweetalert::alert', ['cdn' => "https://cdn.jsdelivr.net/npm/sweetalert2@9"])
+
 @section('scripts')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
 
