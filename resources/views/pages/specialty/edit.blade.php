9996fc84fafc4c995b2b2ad3ac338a9acdac1577
islamshu12 <islamshu12@gmail.com>
2022-12-28 12:00:51 +0200

    edit

--- resources/views/pages/specialty/edit.blade.php
+++ resources/views/pages/specialty/edit.blade.php
@@ -36,6 +36,8 @@
     </form>
 
 </div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
 
 <script>
