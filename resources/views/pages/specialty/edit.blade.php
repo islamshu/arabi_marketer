23844c282bb0ad1174c7aaf219fff9bd9cf9d1ce
islamshu12 <islamshu12@gmail.com>
2022-12-28 12:04:57 +0200

    edit

--- resources/views/pages/specialty/edit.blade.php
+++ resources/views/pages/specialty/edit.blade.php
@@ -39,6 +39,9 @@
 <script src="{{ asset('new_js/crud.js') }}"></script>
 <script src="{{ asset('new_js/upload_file.js') }}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
+</script>
 
 <script>
    
