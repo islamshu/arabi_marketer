1c41229960bb8403f58d9c51dc8b812032badba6
islamshu12 <islamshu12@gmail.com>
2022-12-28 11:53:42 +0200

    edit

--- resources/views/pages/specialty/edit.blade.php
+++ resources/views/pages/specialty/edit.blade.php
@@ -2,7 +2,7 @@
 <div class="modal-body ">
     <div id="form-errors" class="text-center"></div>
     <div id="success" class="text-center"></div>
-    <form  enctype="multipart/form-data">
+    <form id="edit_form_new" enctype="multipart/form-data">
         @csrf
 
         <div class="form-group">
@@ -32,10 +32,10 @@
         <br>
 
 
-        <button class="btn btn-info" type="button">تعديل</i></button>
+        <button class="btn btn-info" type="submit">تعديل</i></button>
     </form>
 
-
+    
 </div>
 @section('scripts')
 
@@ -43,9 +43,7 @@
 
 <script>
    
-    $('#edit_form_new').click(function(e) {
-        
-        alert('ff');
+    $('#edit_form_new').on('submit', function(e) {
         e.preventDefault();
         var frm = $('#edit_form_new');
         var formData = new FormData(frm[0]);
