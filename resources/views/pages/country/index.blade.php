d8fc12084b03d2fce3e5b75444ebbc5684f8bdee
islamshu12 <islamshu12@gmail.com>
2022-12-31 16:00:52 +0200

    edit

--- resources/views/pages/country/index.blade.php
+++ resources/views/pages/country/index.blade.php
@@ -56,6 +56,7 @@
 
                 <div class="col-md-8" id="form_toshow" style="display: none;margin-top:5px">
                     <form method="post" action="{{ route('countires.store_data') }}" enctype="multipart/form-data">
+                        @csrf
                         <div class="form-body">
                             <div class="row">
                                 <div class="col-md-8">
