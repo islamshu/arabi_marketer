a5d00ffa1788c0ed54b714f1386e9273453a1199
islamshu12 <islamshu12@gmail.com>
2023-01-01 14:12:26 +0200

    edit

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -76,11 +76,10 @@ class UserController extends BaseController
         });
 
 
-        $blogs = $query->where('publish_time','<=',now())->orderby('id', 'desc')->paginate(6);
-        // $blogs = $query->paginate(6);
+        $blogs = $query->paginate(9);
 
-        $res = BlogResource::collection($blogs)->response()->getData(true);
-        return $this->sendResponse($res, 'جميع المقالات');
+        $res = UserResource::collection($blogs)->response()->getData(true);
+        return $this->sendResponse($res, 'جميع صناع المحتوى');
     }
     
     
