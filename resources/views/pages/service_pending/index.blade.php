8e36f56aebcc2d9f29903f8897f73ee3ff8ac5a5
islamshu12 <islamshu12@gmail.com>
2023-01-05 12:52:58 +0200

    edit

--- resources/views/pages/service_pending/index.blade.php
+++ resources/views/pages/service_pending/index.blade.php
@@ -10,7 +10,7 @@
                 role="tablist">
                 <!--begin:::Tab item-->
                 <li class="nav-item" role="presentation">
-                    <a class="nav-link text-active-primary pb-5 " href="/" >
+                    <a class="nav-link text-active-primary pb-5 " href="/">
                         <!--begin::Svg Icon | path: icons/duotune/general/gen001.svg-->
                         <span class="svg-icon svg-icon-2 me-2">
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
@@ -25,13 +25,18 @@
 
                 </li>
                 <li class="nav-item" role="presentation">
-                    <a class="nav-link text-active-primary pb-5 active" >
+                    <a class="nav-link text-active-primary pb-5 active">
                         <!--begin::Svg Icon | path: icons/duotune/general/gen001.svg-->
                         <span class="svg-icon svg-icon-2 me-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                <path opacity="0.3" d="M21 22H14C13.4 22 13 21.6 13 21V3C13 2.4 13.4 2 14 2H21C21.6 2 22 2.4 22 3V21C22 21.6 21.6 22 21 22Z" fill="currentColor"></path>
-                                <path d="M10 22H3C2.4 22 2 21.6 2 21V3C2 2.4 2.4 2 3 2H10C10.6 2 11 2.4 11 3V21C11 21.6 10.6 22 10 22Z" fill="currentColor"></path>
-                                </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none">
+                                <path opacity="0.3"
+                                    d="M21 22H14C13.4 22 13 21.6 13 21V3C13 2.4 13.4 2 14 2H21C21.6 2 22 2.4 22 3V21C22 21.6 21.6 22 21 22Z"
+                                    fill="currentColor"></path>
+                                <path
+                                    d="M10 22H3C2.4 22 2 21.6 2 21V3C2 2.4 2.4 2 3 2H10C10.6 2 11 2.4 11 3V21C11 21.6 10.6 22 10 22Z"
+                                    fill="currentColor"></path>
+                            </svg>
                         </span>
                         <!--end::Svg Icon-->الخدمات
                     </a>
@@ -40,7 +45,7 @@
 
 
             </ul>
-            
+
 
             <ul class="nav nav-tabs nav-line-tabs nav-line-tabs-2x mb-5 fs-6">
                 <li class="nav-item">
@@ -55,16 +60,16 @@
                 <li class="nav-item">
                     <a class="nav-link " data-bs-toggle="tab" href="#kt_tab_pane_4"> الفيديوهات</a>
                 </li>
-            
-            
-                
+
+
+
             </ul>
 
-            
+
             <div class="tab-content" id="myTabContent">
                 <div class="tab-pane fade show active" id="kt_tab_pane_1" role="tabpanel">
                     <div>
-                        <table id="eexdampleee"  class="display example" style="width:100%">
+                        <table id="example_service" class="display example" style="width:100%">
                             <thead>
                                 <tr>
                                     <th>صورة الخدمة</th>
@@ -73,38 +78,40 @@
                                     <th>تاريخ الاضافة</th>
                                     <th>نوع المستخدم</th>
                                     <th> الحالة</th>
-                    
+
                                     <th>العمليات</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 @foreach ($services as $item)
-                                <tr>
-                                 <td><img src="{{ asset('public/uploads/'.$item->image) }}" width="50" height="50" alt=""></td>
-                                 <td>{{ $item->title }}</td>
-                                 <th><a href="{{ route('marketer.show',$item->user->id) }}">{{ $item->user->name }}</a></th>
-                                 <td>{{ date('Y-m-d', strtotime($item->created_at)); }}</td>
-                               <td>  {{ $item->user->type }}</td>
-                                 <td>
-                                    <input type="checkbox" data-id="{{ $item->id }}" name="status" class="js-switch allsseeeee"
-                                        {{ $item->status == 1 ? 'checked' : '' }}>
-                                </td>
-                                 <td>
-                                    <a href="{{ route('services.show', $item->id) }}" class="btn btn-success"><i class="fa fa-eye"></i></a>
-                    
-                                    {{-- <a href="{{ route('services.edit', $item->id) }}" class="btn btn-info"><i class="fa fa-edit"></i></a> --}}
-                                    <form style="display: inline"
-                                        action="{{ route('services.destroy', $item->id) }}"
-                                        method="post">
-                                        @method('delete') @csrf
-                                        <button type="submit" class="btn btn-danger delete-confirm"><i
-                                                class="fa fa-trash"></i></button>
-                                    </form>
-                                </td>
-                                </tr>
-                                    
+                                    <tr>
+                                        <td><img src="{{ asset('public/uploads/' . $item->image) }}" width="50"
+                                                height="50" alt=""></td>
+                                        <td>{{ $item->title }}</td>
+                                        <th><a
+                                                href="{{ route('marketer.show', $item->user->id) }}">{{ $item->user->name }}</a>
+                                        </th>
+                                        <td>{{ date('Y-m-d', strtotime($item->created_at)) }}</td>
+                                        <td> {{ $item->user->type }}</td>
+                                        <td>
+                                            <input type="checkbox" data-id="{{ $item->id }}" name="status"
+                                                class="js-switch" {{ $item->status == 1 ? 'checked' : '' }}>
+                                        </td>
+                                        <td>
+                                            <a href="{{ route('services.show', $item->id) }}" class="btn btn-success"><i
+                                                    class="fa fa-eye"></i></a>
+
+                                            {{-- <a href="{{ route('services.edit', $item->id) }}" class="btn btn-info"><i class="fa fa-edit"></i></a> --}}
+                                            <form style="display: inline"
+                                                action="{{ route('services.destroy', $item->id) }}" method="post">
+                                                @method('delete') @csrf
+                                                <button type="submit" class="btn btn-danger delete-confirm"><i
+                                                        class="fa fa-trash"></i></button>
+                                            </form>
+                                        </td>
+                                    </tr>
                                 @endforeach
-                            </tfoot>
+                                </tfoot>
                         </table>
                     </div>
                 </div>
@@ -124,19 +131,25 @@
                             <tbody>
                                 @foreach ($blogs as $item)
                                     <tr>
-                                        <td><img src="{{ asset('public/uploads/' . $item->image_blog->image) }}" width="50" height="50"
-                                                alt=""></td>
+                                        <td><img src="{{ asset('public/uploads/' . $item->image_blog->image) }}"
+                                                width="50" height="50" alt=""></td>
                                         <td>{{ $item->title }}</td>
-                                        <th><a href="{{ route('marketer.show', $item->user->id) }}">{{ $item->user->name }}</a></th>
-                                        <th><a href="{{ route('show_comments', $item->id) }}">{{ $item->comments->count() }}</a> </th>
-                       
+                                        <th><a
+                                                href="{{ route('marketer.show', $item->user->id) }}">{{ $item->user->name }}</a>
+                                        </th>
+                                        <th><a
+                                                href="{{ route('show_comments', $item->id) }}">{{ $item->comments->count() }}</a>
+                                        </th>
+
                                         <td>
-                                            <input type="checkbox" data-id="{{ $item->id }}" name="status" class="js-switch"
-                                                {{ $item->status == 1 ? 'checked' : '' }}>
+                                            <input type="checkbox" data-id="{{ $item->id }}" name="status"
+                                                class="js-switch" {{ $item->status == 1 ? 'checked' : '' }}>
                                         </td>
                                         <td>
-                                            <a href="{{ route('blogs.edit', $item->id) }}" class="btn btn-info"><i class="fa fa-edit"></i></a>
-                                            <form style="display: inline" action="{{ route('blogs.destroy', $item->id) }}" method="post">
+                                            <a href="{{ route('blogs.edit', $item->id) }}" class="btn btn-info"><i
+                                                    class="fa fa-edit"></i></a>
+                                            <form style="display: inline"
+                                                action="{{ route('blogs.destroy', $item->id) }}" method="post">
                                                 @method('delete') @csrf
                                                 <button type="submit" class="btn btn-danger delete-confirm"><i
                                                         class="fa fa-trash"></i></button>
@@ -154,10 +167,10 @@
                             <thead>
                                 <tr>
                                     <th>صورة صناع المحتوى </th>
-            
+
                                     <th>اسم صناع المحتوى </th>
                                     <th>البريد الالكتروني </th>
-                                   
+
                                     <th>تاريخ الاضافة</th>
                                     <th>العمليات</th>
                                 </tr>
@@ -165,8 +178,8 @@
                             <tbody>
                                 @foreach ($users as $item)
                                     <tr>
-                                        <td><img src="{{ asset('public/uploads/' . $item->image) }}" width="50" height="50"
-                                                alt=""></td>
+                                        <td><img src="{{ asset('public/uploads/' . $item->image) }}" width="50"
+                                                height="50" alt=""></td>
                                         <td>{{ $item->name }}</td>
                                         <td>{{ $item->email }}</td>
                                         {{-- @can('edit-status-marketers')
@@ -178,9 +191,9 @@
                                         @endcan --}}
                                         <td>{{ date('Y-m-d', strtotime($item->created_at)) }}</td>
                                         <td>
-            
-                                            <a href="{{ route('show_customer_markter', $item->id) }}" class="btn btn-success"><i
-                                                    class="fa fa-eye"></i></a>
+
+                                            <a href="{{ route('show_customer_markter', $item->id) }}"
+                                                class="btn btn-success"><i class="fa fa-eye"></i></a>
                                             {{-- <a href="{{ route('services.edit', $item->id) }}" class="btn btn-info"><i
                                                     class="fa fa-edit"></i></a>
             
@@ -223,7 +236,8 @@
                                                 href="{{ route('marketer.show', $item->user->id) }}">{{ $item->user->name }}</a>
                                         </th>
 
-                                        <th><a href="{{ $item->url }}" target="_blacnk"><i class="fa fa-eye"></i></a>
+                                        <th><a href="{{ $item->url }}" target="_blacnk"><i
+                                                    class="fa fa-eye"></i></a>
                                         </th>
                                         <td>{{ date('Y-m-d', strtotime($item->created_at)) }}</td>
                                         <td>
@@ -262,13 +276,13 @@
 
 
 
-                
-             
-              
+
+
+
             </div>
             <!--end:::Tabs-->
             <!--begin:::Tab content-->
-            
+
             <!--end:::Tab content-->
         </div>
         <!--end::Card body-->
@@ -290,28 +304,77 @@
     
 @endphp --}}
 
-@include('sweetalert::alert', ['cdn' => "https://cdn.jsdelivr.net/npm/sweetalert2@9"])
+@include('sweetalert::alert', ['cdn' => 'https://cdn.jsdelivr.net/npm/sweetalert2@9'])
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
 <script>
-      $(document).ready(function() {
-                    $("#eexdampleee").on("change", ".allsseeeee", function() {
-                        let status = $(this).prop('checked') === true ? 1 : 0;
-                        let userId = $(this).data('id');
-                        $.ajax({
-                            type: "GET",
-                            dataType: "json",
-                            url: '{{ route('service.update.status') }}',
-                            data: {
-                                'status': status,
-                                'serviceId': userId
-                            },
-                            success: function(data) {
-                                console.log(data.message);
-                            }
-                        });
-                    });
-                });
-</script>
+    $(document).ready(function() {
+        $("#example_service").on("change", ".js-switch", function() {
+            let status = $(this).prop('checked') === true ? 1 : 0;
+            let userId = $(this).data('id');
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                url: '{{ route('service.update.status') }}',
+                data: {
+                    'status': status,
+                    'serviceId': userId
+                },
+                success: function(data) {
+                    console.log(data.message);
+                }
+            });
+        });
+        $("#example_blog").on("change", ".js-switch", function() {
+            let status = $(this).prop('checked') === true ? 1 : 0;
+            let userId = $(this).data('id');
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                url: '{{ route('blogs.update.status') }}',
+                data: {
+                    'status': status,
+                    'serviceId': userId
+                },
+                success: function(data) {
+                    console.log(data.message);
+                }
+            });
+        });
+    });
 
- 
+    function myFunctionf(id) {
+
+        let status = $('#worker_status_' + id).val();
+        let booked_id = id;
+        $.ajax({
+            type: 'post',
+            url: "{{ route('update_status_video') }}",
+            data: {
+                "_token": "{{ csrf_token() }}",
+                'status': status,
+                'booked_id': booked_id,
+            },
+            beforeSend: function() {},
+            success: function(data) {
+                if (data['status'] == true) {
+                    if (status == 1) {
+                        $('#worker_status_' + id).css("backgroundColor", "#5fc69e")
+                    } else if (status == 0) {
+                        $('#worker_status_' + id).css("backgroundColor", "#a1a5b7")
+                    } else if (status == 2) {
+                        $('#worker_status_' + id).css("backgroundColor", "#f1416c")
+                    }
+                    toastr.options.closeButton = true;
+                    toastr.options.closeMethod = 'fadeOut';
+                    toastr.options.closeDuration = 100;
+                    toastr.success('{{ __('Updated successfully') }}');
+
+                } else {
+                    alert('Whoops Something went wrong!!');
+                }
+
+            }
+        });
+    }
+</script>
