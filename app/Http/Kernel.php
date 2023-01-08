a8774a5fd7b818e775d0d60965d71d958fc75b02
islamshu12 <islamshu12@gmail.com>
2023-01-08 11:16:34 +0200

    edit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -24,7 +24,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\GzipMiddleware::class,
 
 
     ];
@@ -49,6 +48,8 @@ class Kernel extends HttpKernel
 
         'api' => [
             'throttle:api',
+            \App\Http\Middleware\GzipMiddleware::class,
+
             // \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
