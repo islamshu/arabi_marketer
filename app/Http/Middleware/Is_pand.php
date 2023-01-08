23d19cc53f3c12497ef16f10e61a6d0931c7cc2a
islamshu12 <islamshu12@gmail.com>
2023-01-08 11:49:14 +0200

    edit

---
+++ app/Http/Middleware/Is_pand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Is_pand
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    
+    public function handle($request, Closure $next)
+    {
+        if (auth('api')->check()) {
+            if(auth('api')->user()->is_pan != 1){
+                return $next($request);
+            }else{
+                $response = ['success' => false, 'message' => 'your are pan','code'=>400];
+                if (!empty($errorMessages))
+                    $response['data'] = $errorMessages;
+                return response()->json($response , 200);
+            }
+        }else{
+            $response = ['success' => false, 'message' => 'you need to login','code'=>400];
+            if (!empty($errorMessages))
+                $response['data'] = $errorMessages;
+            return response()->json($response , 200);
+        }
+       
+      }
+    
+        
+        
+        
+    
+    
+}
