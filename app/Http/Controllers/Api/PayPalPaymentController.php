8817e19185d46940f7f9fabfb07781f850dcfaec
islamshu12 <islamshu12@gmail.com>
2023-01-12 14:08:59 +0200

    edit

--- app/Http/Controllers/Api/PayPalPaymentController.php
+++ app/Http/Controllers/Api/PayPalPaymentController.php
@@ -141,9 +141,9 @@ class PayPalPaymentController extends BaseController
             foreach($carts as $cart){
                 $cart->delete();
             }
-
-            $userorder = User::find($order->user_id);
-            dd( $order = Order::find($id));
+            $ordermail = Order::find($id);
+            $userorder = User::find($ordermail->user_id);
+            dd( $userorder);
             Mail::to($userorder->email)->send(new OrderMail($order->id));
 
 
