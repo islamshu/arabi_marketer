33bc1041186d07b5bf799c413574b2aa6f9bd04a
islamshu12 <islamshu12@gmail.com>
2023-01-14 16:37:01 +0200

    edit

--- app/Http/Controllers/Api/MessageController.php
+++ app/Http/Controllers/Api/MessageController.php
@@ -13,8 +13,8 @@ class MessageController extends BaseController
 {
     public function get_count(){
         $message = Message::where('receiver_id',auth('api')->id())->where('seen_receiver',0)->get();
-        $unique = $message->unique('sender_id');
-        dd($unique);
+        // $unique = $message->unique('sender_id');
+        dd($message);
 
         return $this->sendResponse($message , 'count of message' .auth('api')->id() );
     }
