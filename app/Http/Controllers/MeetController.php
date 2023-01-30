5bc9fbe6618d20c2db94d0c37d5cbc1dd42d43c7
islamshu12 <islamshu12@gmail.com>
2023-01-30 14:04:21 +0200

    edit

--- app/Http/Controllers/MeetController.php
+++ app/Http/Controllers/MeetController.php
@@ -21,6 +21,7 @@ class MeetController extends Controller
         $client->setApplicationName("Google Meet Laravel");
         // $client->setScopes([Google_Service_Calendar::CALENDAR, Google_Service_Calendar::CALENDAR_EVENTS]);
         $privateKeyJsonFile = public_path("private_key.json");
+        dd($privateKeyJsonFile);
 
         $client->setAuthConfig($privateKeyJsonFile);
         $service = new Google_Service_Calendar($client);
