3eb9fe950e0c8ec57aae236bba9e93c96a4f4485
islamshu12 <islamshu12@gmail.com>
2023-02-01 16:21:58 +0200

    edit

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -69,7 +69,7 @@ class MeetingController extends Controller
             'timezone'     => 'Asia/Riyadh',
             'settings'   => [
                 'waiting_room'      => false,
-                "alternative_hosts"=>"jchill@example.com;thill@example.com",
+                "alternative_hosts"=>"jchill@example.com;testmail24444@gmail.com",
 
             ],
         ]);
