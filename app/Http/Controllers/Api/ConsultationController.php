6d40e1deace6bccfcb079382a1ae13ca15ed78df
islamshu12 <islamshu12@gmail.com>
2023-02-04 12:51:50 +0200

    edit

--- app/Http/Controllers/Api/ConsultationController.php
+++ app/Http/Controllers/Api/ConsultationController.php
@@ -107,10 +107,10 @@ class ConsultationController extends BaseController
                     $date =[];
                     foreach ($period as $key=>$time) {
                         $dateeformat = Carbon::createFromFormat('Y-m-d H:i:s', $time->format('Y-m-d H:i:s'));
-                       $t= $dateeformat->timezone = 'Asia/Riyadh';
+                      $dateeformat->timezone = 'Asia/Riyadh';
 
                         // $datee = $dateeformat->toIso8601String();
-                        array_push($date,$t); 
+                        array_push($date,$dateeformat); 
                     }
                     $day =[];
                     $newData = [];
