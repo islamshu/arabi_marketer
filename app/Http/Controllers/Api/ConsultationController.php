80968e6a4d47ff27c13aede8f784ffebea1a9cd0
islamshu12 <islamshu12@gmail.com>
2023-02-04 11:48:09 +0200

    edit

--- app/Http/Controllers/Api/ConsultationController.php
+++ app/Http/Controllers/Api/ConsultationController.php
@@ -82,11 +82,10 @@ class ConsultationController extends BaseController
             $name_of_day = $d->format('l');
             if(in_array($name_of_day,json_decode($cons->user->con_user->dates) )){
                 $datee =$d->format('Y-m-d H:i:s');
-                dd($datee);
                 $dateeformat = Carbon::createFromFormat('Y-m-d H:i:s', $datee);
                 // $dateeformat->timezone = 'Asia/Kolkata';
                 // $datee = $dateeformat->toIso8601String();
-                $json_date['date']=$dateeformat;
+                $json_date['date']=$datee;
                 $json_date['name_this_daye']=$d->format('l');
 
                 // dd($d->format('Y-m-d H:i:s'));
