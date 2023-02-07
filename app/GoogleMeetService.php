1e97697b4efb9479e79df7b185f1ea79848e284d
islamshu12 <islamshu12@gmail.com>
2023-02-07 19:02:50 +0200

    edit

--- app/GoogleMeetService.php
+++ app/GoogleMeetService.php
@@ -19,7 +19,7 @@ class GoogleMeetService
         $this->client->setApplicationName('Google Meet Integration');
         $this->client->setScopes(Google_Service_Calendar::CALENDAR);
         $this->client->setAuthConfig(config_path('google_api_credentials.json'));
-        
+
         $this->client->setAccessType('offile');
         $this->client->setPrompt('select_account consent');
     }
@@ -29,31 +29,42 @@ class GoogleMeetService
         return $this->client;
     }
     public function authenticate()
-{
-    $this->client->authenticate($_GET['code']);
-    $accessToken = $this->client->getAccessToken();
-    $this->client->setAccessToken($accessToken);
-}
-public function createMeet($summary, $description, $startTime, $endTime)
-{
-    $calendarService = new Google_Service_Calendar($this->client);
+    {
+        $this->client->authenticate($_GET['code']);
+        $accessToken = $this->client->getAccessToken();
+        $this->client->setAccessToken($accessToken);
+    }
+    public function createMeet($summary, $description, $startTime, $endTime)
+    {
+        $calendarService = new Google_Service_Calendar($this->client);
 
-// Create new Calendar Event
-$event = new Google_Service_Calendar_Event();
-$event->setSummary("Example Meeting");
-$event->setDescription("This is an example meeting created with Google Calendar API");
-$start = new Google_Service_Calendar_EventDateTime();
-$start->setDateTime("2023-02-07T10:00:00+00:00");
-$start->setTimeZone("UTC");
-$event->setStart($start);
-$end = new Google_Service_Calendar_EventDateTime();
-$end->setDateTime("2023-02-07T11:00:00+00:00");
-$end->setTimeZone("UTC");
-$event->setEnd($end);
-$calendarId = "primary";
-$event = $calendarService->events->insert($calendarId, $event);
-$joinUrl = $event->htmlLink;
-dd($joinUrl);
+        // Define the calendar ID
+        $calendarId = 'primary';
 
-}
+        // Create the Google Meet event
+        $event = new Google_Service_Calendar_Event();
+        $event->setSummary("Test Meeting");
+        $event->setLocation("Online");
+        $start = new Google_Service_Calendar_EventDateTime();
+        $start->setDateTime("2023-02-07T12:00:00+05:30");
+        $start->setTimeZone("Asia/Kolkata");
+        $event->setStart($start);
+        $end = new Google_Service_Calendar_EventDateTime();
+        $end->setDateTime("2023-02-07T13:00:00+05:30");
+        $end->setTimeZone("Asia/Kolkata");
+        $event->setEnd($end);
+
+        // Add attendees to the event
+        $attendee1 = new Google_Service_Calendar_EventAttendee();
+        $attendee1->setEmail("attendee1@example.com");
+        $attendees = array($attendee1);
+        $event->attendees = $attendees;
+
+        // Insert the event on the calendar
+        $event = $calendarService->events->insert($calendarId, $event);
+
+        // Get the join URL for the meeting
+        $joinUrl = $event->getJoinUrl();
+        return $joinUrl;
+    }
 }
