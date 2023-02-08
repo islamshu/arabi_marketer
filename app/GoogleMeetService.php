4eb175ddc2e5f24f3c347ad6a3cc097ba072bab1
islamshu12 <islamshu12@gmail.com>
2023-02-08 13:31:09 +0200

    edit

--- app/GoogleMeetService.php
+++ app/GoogleMeetService.php
@@ -63,12 +63,12 @@ class GoogleMeetService
                 'dateTime' => '2023-02-08T11:00:00-07:00',
                 'timeZone' => 'America/Los_Angeles',
             ],
-            'attendees' => [
-                [
-                    'email' => 'islamshu12@gmail.com',
-                ],
+            // 'attendees' => [
+            //     [
+            //         'email' => 'islamshu12@gmail.com',
+            //     ],
                
-            ],
+            // ],
             'conferenceData' => [
                 'createRequest' => [
                     'requestId' => uniqid(),
