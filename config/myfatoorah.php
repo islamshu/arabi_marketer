c66a5f2a3c01f8dc527b5110bc6fd5743c340847
islamshu12 <islamshu12@gmail.com>
2023-02-08 15:24:57 +0200

    Edit

--- config/myfatoorah.php
+++ config/myfatoorah.php
@@ -7,12 +7,12 @@ return [
      * Live Token: https://myfatoorah.readme.io/docs/live-token
      * Test Token: https://myfatoorah.readme.io/docs/test-token
      */
-    'api_key' => 'x4DpfiXGd_vJ9jfQFvw-0E_TE_ihxXB8dHN4m4RIJUn5csorsLLi8FdNpHtvmbBBO2F-DCCx-6-FqjsNts1C90uTZkzQCCbX8RSom51FfEL09P6UAtMZ3LuN07KES4K5131wBe4WY4cVmid3dUK8GRUSGf7d9Q83yrxyU9JRfGj1cl-zJP0wxL5wOm0ZyhEnjCfp29YStiWAVAqbS6PbkyCW1fKiLBPwDxHR2aE869GwqZ1kF-y-5nzCfnu5f8dkVSzk-E5TxuTxXeL-BQlbLPA3DuG3aujqjC5PtbhsDRpRUOJQ7_2GSACAc1oa7Iimy_YsUgPdvnyByle7vXL3gZNZqP2LfckNUvktyzq0HYfa7Zp2P6eWpCsOlNkIyHjQGqHLgCzrmRpd48PrP4npPtdGPHK8E2huZVoJ-EKw7Wr5-Z1IiaJh83HYIs2RRLZBl5Rpb7AvoCZSoarI9KgquehieBnGIaY0RhaRK9RYy36Z3eFpSXWx6umoWamp4mFPeHAd0qCl1jsd-7zZO-4bFfAXy9SQzkvCgwLivJqAcygEyK5k7Ldm_9J_BSTDX9-5brz-DLEbIzWIC_H8eGw6DbxD5IPL5ZBS0Iy7uFVAu6n1cUnNsALPXZI-dxRbbpRW15xz1noOserYWDx_84Z4PyFp9RGoxN_eq5_SZSkvjL-zHPkt9X2BBdZtD9clmJebNMFRUg',
+    'api_key' => 'rLtt6JWvbUHDDhsZnfpAhpYk4dxYDQkbcPTyGaKp2TYqQgG7FGZ5Th_WD53Oq8Ebz6A53njUoo1w3pjU1D4vs_ZMqFiz_j0urb_BH9Oq9VZoKFoJEDAbRZepGcQanImyYrry7Kt6MnMdgfG5jn4HngWoRdKduNNyP4kzcp3mRv7x00ahkm9LAK7ZRieg7k1PDAnBIOG3EyVSJ5kK4WLMvYr7sCwHbHcu4A5WwelxYK0GMJy37bNAarSJDFQsJ2ZvJjvMDmfWwDVFEVe_5tOomfVNt6bOg9mexbGjMrnHBnKnZR1vQbBtQieDlQepzTZMuQrSuKn-t5XZM7V6fCW7oP-uXGX-sMOajeX65JOf6XVpk29DP6ro8WTAflCDANC193yof8-f5_EYY-3hXhJj7RBXmizDpneEQDSaSz5sFk0sV5qPcARJ9zGG73vuGFyenjPPmtDtXtpx35A-BVcOSBYVIWe9kndG3nclfefjKEuZ3m4jL9Gg1h2JBvmXSMYiZtp9MR5I6pvbvylU_PP5xJFSjVTIz7IQSjcVGO41npnwIxRXNRxFOdIUHn0tjQ-7LwvEcTXyPsHXcMD8WtgBh-wxR8aKX7WPSsT1O8d8reb2aR7K3rkV3K82K_0OgawImEpwSvp9MNKynEAJQS6ZHe_J_l77652xwPNxMRTMASk1ZsJL',
     /**
      * Test Mode (boolean)
      * Accepted value: true for the test mode or false for the live mode
      */
-    'test_mode' => false,
+    'test_mode' => true,
     /**
      * Country ISO Code (string)
      * Accepted value: KWT, SAU, ARE, QAT, BHR, OMN, JOD, or EGY.
