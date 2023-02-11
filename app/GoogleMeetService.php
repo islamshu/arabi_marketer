ddab39ffac8491a349cead781415cf8e68d99a0b
islamshu12 <islamshu12@gmail.com>
2023-02-11 21:50:23 +0200

    edit

--- app/GoogleMeetService.php
+++ app/GoogleMeetService.php
@@ -10,21 +10,27 @@ use Google_Service_Calendar_EventDateTime;
 use Google\Apis\Meet\v1\MeetService;
 use Google_Service_HangoutsMeet;
 use Google_Service_HangoutsMeet_Meeting;
+
 class GoogleMeetService
 {
     private $client;
 
     public function __construct()
     {
-        $this->client = new Google_Client();
-        $this->client->setApplicationName('Google Meet Integration');
-        // $this->client->setScopes(Google_Service_Calendar::CALENDAR);
-        $this->client->addScope([\Google_Service_Calendar::CALENDAR, \Google_Service_Calendar::CALENDAR_EVENTS]);
+        // $this->client = new Google_Client();
+        // $this->client->setApplicationName('Google Meet Integration');
+        // // $this->client->setScopes(Google_Service_Calendar::CALENDAR);
+        // $this->client->addScope([\Google_Service_Calendar::CALENDAR, \Google_Service_Calendar::CALENDAR_EVENTS]);
 
+        // $this->client->setAuthConfig(config_path('google_api_credentials.json'));
+        // $this->client->setAccessType('offline');
+        // $this->client->setPrompt('select_account consent');
+        $this->client = new Google_Client();
         $this->client->setAuthConfig(config_path('google_api_credentials.json'));
         $this->client->setAccessType('offline');
-        $this->client->setPrompt('select_account consent');
-
+        $this->client->setSubject('islamshu12@gmail.com');
+        $this->client->setApplicationName("YourApplicationName");
+        $this->client->setScopes([\Google_Service_Calendar::CALENDAR, \Google_Service_Calendar::CALENDAR_EVENTS]);
     }
 
     public function getClient()
@@ -48,9 +54,9 @@ class GoogleMeetService
                 'email' => 'islamshu12@gmail.com',
                 'responseStatus' => 'accepted'
             ],
-          
+
         ];
-        
+
         $event = new Google_Service_Calendar_Event([
             'summary' => 'Test Meeting',
             'location' => 'Online',
@@ -68,17 +74,17 @@ class GoogleMeetService
                     'email' => 'islamshu12@gmail.com',
                 ],
             ],
-            
-           
+
+
             'conferenceData' => [
                 'createRequest' => [
-                  'conferenceSolutionKey' => [
-                    'type' => 'hangoutsMeet'
-                  ],
-                  'requestId' => 'randomString123'
+                    'conferenceSolutionKey' => [
+                        'type' => 'hangoutsMeet'
+                    ],
+                    'requestId' => 'randomString123'
                 ]
             ],
-            
+
             // 'attendees' => $attendees,
             'reminders' => [
                 'useDefault' => FALSE,
@@ -101,7 +107,5 @@ class GoogleMeetService
         $calendarId = env('GOOGLE_CALENDAR_ID');
         $event = $calendarService->events->patch($calendarId, $event->id, $event, ['conferenceDataVersion' => 1]);
         return $event;
-
-      
     }
 }
