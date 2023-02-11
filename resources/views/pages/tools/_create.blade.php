933aca89ac384127a37cf8045ad8077b1f5f349e
islamshu12 <islamshu12@gmail.com>
2023-02-11 16:42:31 +0200

    edit

--- resources/views/pages/tools/_create.blade.php
+++ resources/views/pages/tools/_create.blade.php
@@ -18,34 +18,45 @@
 
         </div>
 
-     
-     
+
+
         <div class="form-group col-md-6">
 
-            <br> <label>رابط الخدمة:</label>
+            {{-- <br> <label>رابط الخدمة:</label>
             <input type="url" name="link" id="link"  class="form-control form-control-solid"
-                placeholder="رابط الاداة" />
+                placeholder="رابط الاداة" /> --}}
+            <table class="table table-bordered" id="dynamicAddRemove">
+                <tr>
+                    <th>Title</th>
+                    <th>Action</th>
+                </tr>
+                <tr>
+                    <td><input type="text" name="moreFields[0][title]" placeholder="Enter title"
+                            class="form-control" /></td>
+                    <td><button type="button" name="add" id="add-btn" class="btn btn-success">Add More</button>
+                    </td>
+                </tr>
+            </table>
         </div>
 
-       
+
+
         <div class=" col-md-12">
             <div class="form-group">
                 <br> <label data-error="wrong" data-success="right" for="form3"> صور عن الخدمة <span
                         class="required"></span></label>
                 {{-- <input type="file" multiple id="imageupload" name="images[]" class="form-control"> --}}
-                <input id="imagestore" class="form-control image" required type="file" name="image" ><br />
-     
+                <input id="imagestore" class="form-control image" required type="file" name="image"><br />
+
 
             </div>
 
         </div>
-        <input type="button" value="Preview form" class="add" id="preview" />
-<input type="button" value="Add a field" class="add" id="add" />
 
-      
+
     </div>
-   
-     <br>
+
+    <br>
     <button class="btn btn-info" id="submitform" style="" type="submit">اضف جديد </i></button>
 </form>
 
