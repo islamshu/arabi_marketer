bb153a715a6ea8b22dc6e3d7801af50d2716346c
islamshu12 <islamshu12@gmail.com>
2023-02-11 17:37:53 +0200

    edit

--- resources/views/pages/tools/_create.blade.php
+++ resources/views/pages/tools/_create.blade.php
@@ -14,7 +14,7 @@
         <div class="form-group col-md-8">
 
             <br><label> الوصف :</label>
-            <textarea name="description" name="description" required   class="form-control" id=""></textarea>
+            <textarea name="description" name="description" required class="form-control" id=""></textarea>
 
         </div>
         <div class="form-group col-md-6">
@@ -31,12 +31,14 @@
 
 
         </div>
+    </div>
+    <div class="row">
 
 
 
         <div class="form-group col-md-6">
 
-          <br> <label>روابط الاداة :</label>
+            <br> <label>روابط الاداة :</label>
             {{--   <input type="url" name="link" id="link"  class="form-control form-control-solid"
                 placeholder="رابط الاداة" /> --}}
             <table class="table table-bordered" id="dynamicAddRemove">
@@ -49,11 +51,11 @@
                     <td><input type="text" required name="moreFields[0][url]" placeholder="اضف الرابط"
                             class="form-control" /></td>
                     <td>
-                    <select name="moreFields[0][type]" required class="form-control">
-                        <option value="apple">Apple</option>
-                        <option value="google">Google</option>
-                        <option value="url">url</option>
-                    </select>
+                        <select name="moreFields[0][type]" required class="form-control">
+                            <option value="apple">Apple</option>
+                            <option value="google">Google</option>
+                            <option value="url">url</option>
+                        </select>
                     <td>
                         <button type="button" name="add" id="add-btn" class="btn btn-success">Add More</button>
                     </td>
@@ -81,5 +83,3 @@
     <br>
     <button class="btn btn-info" id="submitform" style="" type="submit">اضف جديد </i></button>
 </form>
-
-
