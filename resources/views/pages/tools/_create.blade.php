05d639655b20d3b86b8a2fc7a1afaea9227fee6d
islamshu12 <islamshu12@gmail.com>
2023-02-11 17:31:26 +0200

    edit

--- resources/views/pages/tools/_create.blade.php
+++ resources/views/pages/tools/_create.blade.php
@@ -14,7 +14,7 @@
         <div class="form-group col-md-8">
 
             <br><label> الوصف :</label>
-            <textarea name="description" name="description"   class="form-control" id=""></textarea>
+            <textarea name="description" name="description" required   class="form-control" id=""></textarea>
 
         </div>
 
@@ -32,10 +32,10 @@
                     <th>Action</th>
                 </tr>
                 <tr>
-                    <td><input type="text" name="moreFields[0][url]" placeholder="اضف الرابط"
+                    <td><input type="text" required name="moreFields[0][url]" placeholder="اضف الرابط"
                             class="form-control" /></td>
                     <td>
-                    <select name="moreFields[0][type]" class="form-control">
+                    <select name="moreFields[0][type]" required class="form-control">
                         <option value="apple">Apple</option>
                         <option value="google">Google</option>
                         <option value="url">url</option>
--- resources/views/pages/tools/index.blade.php
+++ resources/views/pages/tools/index.blade.php
@@ -173,7 +173,7 @@
     var i = 0;
     $("#add-btn").click(function(){
     ++i;
-    $("#dynamicAddRemove").append('<tr><td><input type="text" name="moreFields['+i+'][url]" placeholder="أضف رابط" class="form-control" /></td><td><select name="moreFields['+i+'][type]" class="form-control"><option value="apple">Apple</option><option value="google">Google</option><option value="url">url</option></select></td><td><button type="button" class="btn btn-danger remove-tr">Remove</button></td></tr>');
+    $("#dynamicAddRemove").append('<tr><td><input required type="text" name="moreFields['+i+'][url]" placeholder="أضف رابط" class="form-control" /></td><td><select required name="moreFields['+i+'][type]" class="form-control"><option value="apple">Apple</option><option value="google">Google</option><option value="url">url</option></select></td><td><button type="button" class="btn btn-danger remove-tr">Remove</button></td></tr>');
     });
     $(document).on('click', '.remove-tr', function(){  
     $(this).parents('tr').remove();
