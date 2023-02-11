cac958d52673c6aebe2ceb0c0b211976fa61ce88
islamshu12 <islamshu12@gmail.com>
2023-02-11 16:52:57 +0200

    edit

--- resources/views/pages/tools/index.blade.php
+++ resources/views/pages/tools/index.blade.php
@@ -164,14 +164,7 @@
         // Define chart element
     </script>
     <script>
-        ClassicEditor
-            .create(document.querySelector('.editor'))
-            .then(editor => {
-                console.log(editor);
-            })
-            .catch(error => {
-                console.error(error);
-            });
+       
         var input1 = document.querySelector("#kt_tagify_3");
         new Tagify(input1);
     </script>
