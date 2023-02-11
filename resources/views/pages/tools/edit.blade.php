9ea31fad3bf61192702f97c316fe3c294fa08640
islamshu12 <islamshu12@gmail.com>
2023-02-11 18:12:25 +0200

    edit

--- resources/views/pages/tools/edit.blade.php
+++ resources/views/pages/tools/edit.blade.php
@@ -45,7 +45,7 @@
 
 
             </ul>
-            <form method="post" action="{{ route('tools.update',$tool->id) }}" enctype="multipart/form-data">
+            <form method="post" action="{{ route('tools.update', $tool->id) }}" enctype="multipart/form-data">
                 @csrf
                 @method('put')
                 <div class="row">
@@ -54,7 +54,7 @@
 
                         <br><label> عنوان الادة :</label>
 
-                        <input type="text" id="title" name="title"   value="{{ $tool->title }}" required
+                        <input type="text" id="title" name="title" value="{{ $tool->title }}" required
                             class="form-control form-control-solid" placeholder="العنوان الاداة" />
 
                     </div>
@@ -79,18 +79,19 @@
                                 <th>النوع</th>
                                 <th>Action</th>
                             </tr>
-                            @foreach ($tool->links as $key=> $item)
-                            <td><input type="text" value="{{ $item->url }}" required name="moreFields[{{ $key }}][url]" placeholder="اضف الرابط"
-                                class="form-control" /></td>
-                        <td>
-                            <select name="moreFields[{{ $key }}][type]" required class="form-control">
-                                <option value="apple" @if($item->type == 'apple') @endif>Apple</option>
-                                <option value="google" @if($item->type == 'google') @endif>Google</option>
-                                <option value="url" @if($item->type == 'url') @endif>Url</option>
-                            </select>
-                        <td>
-                            <button type="button" name="add" id="add-btn" class="btn btn-success">Add More</button>
-                        </td>
+                            @foreach ($tool->links as $key => $item)
+                                <td><input type="text" value="{{ $item->url }}" required
+                                        name="moreFields[{{ $key }}][url]" placeholder="اضف الرابط"
+                                        class="form-control" /></td>
+                                <td>
+                                    <select name="moreFields[{{ $key }}][type]" required class="form-control">
+                                        <option value="apple" @if ($item->type == 'apple')  @endif>Apple</option>
+                                        <option value="google" @if ($item->type == 'google')  @endif>Google</option>
+                                        <option value="url" @if ($item->type == 'url')  @endif>Url</option>
+                                    </select>
+                                <td>
+                                    <button type="button" class="btn btn-danger remove-tr">Remove</button>
+                                </td>
                             @endforeach
                             <tr>
                                 <td><input type="text" required name="moreFields[0][url]" placeholder="اضف الرابط"
@@ -102,7 +103,9 @@
                                         <option value="url">Url</option>
                                     </select>
                                 <td>
-                                    <button type="button"  id="remove-tr" class="btn btn-delete">Delete</button>
+                                    <button type="button" name="add" id="add-btn" class="btn btn-success">Add
+                                        More</button>
+
                                 </td>
                             </tr>
                         </table>
@@ -116,10 +119,10 @@
                             {{-- <input type="file" multiple id="imageupload" name="images[]" class="form-control"> --}}
                             <input id="imagestore" class="form-control image" required type="file"
                                 name="image"><br />
-                                <div class="form-group">
-                                    <img src="{{ asset('public/uploads/'.$tool->image) }}" style="width: 100px"
-                                        class="img-thumbnail image-preview" alt="">
-                                </div>
+                            <div class="form-group">
+                                <img src="{{ asset('public/uploads/' . $tool->image) }}" style="width: 100px"
+                                    class="img-thumbnail image-preview" alt="">
+                            </div>
 
 
                         </div>
@@ -139,22 +142,22 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
 
 @section('scripts')
-
     <script>
-      
-        
         var input1 = document.querySelector("#kt_tagify_3");
         new Tagify(input1);
     </script>
-       <script type="text/javascript">
+    <script type="text/javascript">
         var i = 0;
-        $("#add-btn").click(function(){
-        ++i;
-        $("#dynamicAddRemove").append('<tr><td><input required type="text" name="moreFields['+i+'][url]" placeholder="أضف رابط" class="form-control" /></td><td><select required name="moreFields['+i+'][type]" class="form-control"><option value="apple">Apple</option><option value="google">Google</option><option value="url">Url</option></select></td><td><button type="button" class="btn btn-danger remove-tr">Remove</button></td></tr>');
+        $("#add-btn").click(function() {
+            ++i;
+            $("#dynamicAddRemove").append('<tr><td><input required type="text" name="moreFields[' + i +
+                '][url]" placeholder="أضف رابط" class="form-control" /></td><td><select required name="moreFields[' +
+                i +
+                '][type]" class="form-control"><option value="apple">Apple</option><option value="google">Google</option><option value="url">Url</option></select></td><td><button type="button" class="btn btn-danger remove-tr">Remove</button></td></tr>'
+                );
         });
-        $(document).on('click', '.remove-tr', function(){  
-        $(this).parents('tr').remove();
-        });  
-        </script>
- 
+        $(document).on('click', '.remove-tr', function() {
+            $(this).parents('tr').remove();
+        });
+    </script>
 @endsection
