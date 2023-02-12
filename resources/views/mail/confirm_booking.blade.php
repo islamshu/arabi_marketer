18cf209f96d5f0382926846c27faa2d9539b4b68
islamshu12 <islamshu12@gmail.com>
2023-02-12 15:31:11 +0200

    edit

--- resources/views/mail/confirm_booking.blade.php
+++ resources/views/mail/confirm_booking.blade.php
@@ -159,7 +159,7 @@
                         <table border="0" cellpadding="0" cellspacing="0">
                           <tr>
                             <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
-                              <a href="{{ $url }}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">افتح الرابط هنا</a>
+                              <a href="{{ $url }}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">اضعط الرابط لمشاهدة الاستشارة  </a>
                             </td>
                           </tr>
                         </table>
@@ -188,7 +188,7 @@
                     <table border="0" cellpadding="0" cellspacing="0">
                       <tr>
                         <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
-                          <a href="{{ $url }}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">افتح الرابط هنا</a>
+                          <a href="{{ $url }}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">افتح الرابط لتأكيد حجز الاستشارة</a>
                         </td>
                       </tr>
                     </table>
