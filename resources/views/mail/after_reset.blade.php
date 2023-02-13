1c567204ba3b8a1de6c1ec2030aff758bf70b2a2
islamshu12 <islamshu12@gmail.com>
2023-02-13 13:28:55 +0200

    edit

--- resources/views/mail/after_reset.blade.php
+++ resources/views/mail/after_reset.blade.php
@@ -1,1 +1,11 @@
-<h1>test</h1>
+<div style="text-align: center">
+    <p style="text-align: center">
+        مرحبا بك!
+    </p>
+    <p style="text-align: center">
+        لقد تم تغير كلمة المرور الخاصة بك بنجاح!
+        
+    </p>
+      
+    
+    </div>
