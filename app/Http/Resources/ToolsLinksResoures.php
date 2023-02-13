52fa3dc52d8ad0ec62b3fe5c488895105a12171d
islamshu12 <islamshu12@gmail.com>
2023-02-13 16:05:16 +0200

    edit

--- app/Http/Resources/ToolsLinksResoures.php
+++ app/Http/Resources/ToolsLinksResoures.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use App\Models\Blog;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class ToolsResoures extends JsonResource
+class ToolsLinksResoures extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -16,12 +16,9 @@ class ToolsResoures extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'=>$this->id,
-            'title'=>$this->title,
-            'description'=>($this->description ),
-            'image'=>asset('public/uploads/'.$this->image),
-            'url'=>ToolsLinksResoures::collection($this->links),
-            
+            'link'=>$this->url,
+            'type'=>$this->type,
+    
         ];
     }
     
