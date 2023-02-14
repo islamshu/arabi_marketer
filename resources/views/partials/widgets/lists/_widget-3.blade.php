550ffe79933d1b1d12c1b0a34420592adfc3d230
islamshu12 <islamshu12@gmail.com>
2023-02-14 14:14:46 +0200

    edit

--- resources/views/partials/widgets/lists/_widget-3.blade.php
+++ resources/views/partials/widgets/lists/_widget-3.blade.php
@@ -1,70 +1,85 @@
-<div class="col-md-6">
-    <div>
-        <table id="eexdample"  class="display example" style="width:100%">
-            <thead>
-                <tr>
-                    <th>صورة </th>
-                    <th>اسم الخدمة</th>
-                    <th>اضيفة بواسطة</th>
-                    <th>تاريخ الاضافة</th>
-    
-                    <th>العمليات</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach (App\Models\Service::take(10)->get() as $item)
-                <tr>
-                 <td><img src="{{ asset('public/uploads/'.$item->image) }}" width="50" height="50" alt=""></td>
-                 <td>{{ $item->title }}</td>
-                 <th><a href="{{ route('marketer.show',$item->user->id) }}">{{ $item->user->name }}</a></th>
-                 <td>{{ date('Y-m-d', strtotime($item->created_at)); }}</td>
-           
-                 <td>
-                    <a href="{{ route('services.show', $item->id) }}" class="btn btn-success"><i class="fa fa-eye"></i></a>
-    
-                    {{-- <a href="{{ route('services.edit', $item->id) }}" class="btn btn-info"><i class="fa fa-edit"></i></a> --}}
-                 
-                </td>
-                </tr>
-                    
-                @endforeach
-            </tfoot>
-        </table>
+<?php
+    // List items
+    $listRows = array(
+        array(
+            'color' => 'success',
+            'title' => 'Create FireStone Logo',
+            'text' => 'Due in 2 Days',
+        ),
+        array(
+            'color' => 'primary',
+            'title' => 'Stakeholder Meeting',
+            'text' => 'Due in 3 Days'
+        ),
+        array(
+            'color' => 'warning',
+            'title' => 'Scoping & Estimations',
+            'text' => 'Due in 5 Days'
+        ),
+        array(
+            'color' => 'primary',
+            'title' => 'KPI App Showcase',
+            'text' => 'Due in 2 Days'
+        ),
+        array(
+            'color' => 'danger',
+            'title' => 'Project Meeting',
+            'text' => 'Due in 12 Days'
+        ),
+        array(
+            'color' => 'success',
+            'title' => 'Customers Update',
+            'text' => 'Due in 1 week'
+        )
+    );
+?>
+
+
+<!--begin::List Widget 3-->
+<div class="card {{ $class }}">
+    <!--begin::Header-->
+    <div class="card-header border-0">
+        <h3 class="card-title fw-bolder text-dark">Todo</h3>
+
+        <div class="card-toolbar">
+            <!--begin::Menu-->
+            <button type="button" class="btn btn-sm btn-icon btn-color-primary btn-active-light-primary" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
+                {!! theme()->getSvgIcon("icons/duotune/general/gen024.svg", "svg-icon-2") !!}
+            </button>
+            {{ theme()->getView('partials/menus/_menu-3') }}
+            <!--end::Menu-->
+        </div>
     </div>
-    
-</div>
-<div class="col-md-6">
-    <div>
-        <table id="eexdample"  class="display example" style="width:100%">
-            <thead>
-                <tr>
-                    <th>صورة الخدمة</th>
-                    <th>اسم الخدمة</th>
-                    <th>اضيفة بواسطة</th>
-                    <th>تاريخ الاضافة</th>
-    
-                    <th>العمليات</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach (App\Models\Service::take(10)->get() as $item)
-                <tr>
-                 <td><img src="{{ asset('public/uploads/'.$item->image) }}" width="50" height="50" alt=""></td>
-                 <td>{{ $item->title }}</td>
-                 <th><a href="{{ route('marketer.show',$item->user->id) }}">{{ $item->user->name }}</a></th>
-                 <td>{{ date('Y-m-d', strtotime($item->created_at)); }}</td>
-              
-                 <td>
-                    <a href="{{ route('services.show', $item->id) }}" class="btn btn-success"><i class="fa fa-eye"></i></a>
-    
-                    {{-- <a href="{{ route('services.edit', $item->id) }}" class="btn btn-info"><i class="fa fa-edit"></i></a> --}}
-                  
-                </td>
-                </tr>
-                    
-                @endforeach
-            </tfoot>
-        </table>
+    <!--end::Header-->
+
+    <!--begin::Body-->
+    <div class="card-body pt-2">
+        @foreach($listRows as $row)
+            <!--begin::Item-->
+            <div class="d-flex align-items-center {{ util()->putIf(next($listRows), 'mb-8') }}">
+                <!--begin::Bullet-->
+                <span class="bullet bullet-vertical h-40px bg-{{ $row['color'] }}"></span>
+                <!--end::Bullet-->
+
+                <!--begin::Checkbox-->
+                <div class="form-check form-check-custom form-check-solid mx-5">
+                    <input class="form-check-input" type="checkbox" value=""/>
+                </div>
+                <!--end::Checkbox-->
+
+                <!--begin::Description-->
+                <div class="flex-grow-1">
+                    <a href="#" class="text-gray-800 text-hover-primary fw-bolder fs-6">{{ $row['title'] }}</a>
+
+                    <span class="text-muted fw-bold d-block">{{ $row['text'] }}</span>
+                </div>
+                <!--end::Description-->
+
+                <span class="badge badge-light-{{ $row['color'] }} fs-8 fw-bolder">New</span>
+            </div>
+            <!--end:Item-->
+        @endforeach
     </div>
-    
+    <!--end::Body-->
 </div>
+<!--end:List Widget 3-->
