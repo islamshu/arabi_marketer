34088376e4428ee61ea56dc70d8ed98f4f51a5fc
islamshu12 <islamshu12@gmail.com>
2023-02-14 16:24:20 +0200

    edit

--- resources/views/partials/widgets/lists/_widget-33.blade.php
+++ resources/views/partials/widgets/lists/_widget-33.blade.php
@@ -84,7 +84,7 @@
     <!--begin::Header-->
     <div class="card-header border-0 pt-5">
         <h3 class="card-title align-items-start flex-column">
-			<span class="card-label fw-bolder fs-3 mb-1">Creator</span>
+			<span class="card-label fw-bolder fs-3 mb-1">Consultion Pending</span>
 
 		</h3>
 
