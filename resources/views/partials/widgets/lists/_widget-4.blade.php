4d2db0998bbb09b4309d1eb6e59822bac3c1efa5
islamshu12 <islamshu12@gmail.com>
2023-02-15 15:52:02 +0200

    edit

--- resources/views/partials/widgets/lists/_widget-4.blade.php
+++ resources/views/partials/widgets/lists/_widget-4.blade.php
@@ -93,7 +93,7 @@
                 <!--begin::Section-->
                 <div class="d-flex align-items-center flex-row-fluid flex-wrap">
                     <div class="flex-grow-1 me-2">
-                        <a href="{{route('services.show',$row->id)}}" class="text-gray-800 text-hover-primary fs-6 fw-bolder">{{ $row->service->title }}</a>
+                        <a href="{{route('services.show',$row->service->id)}}" class="text-gray-800 text-hover-primary fs-6 fw-bolder">{{ $row->service->title }}</a>
 
                     </div>
 
