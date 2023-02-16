53aab289146898930d8191e7d2f44d8a2e610acd
islamshu12 <islamshu12@gmail.com>
2023-02-16 15:12:26 +0200

    edit

--- resources/views/partials/widgets/mixed/_widget-5.blade.php
+++ resources/views/partials/widgets/mixed/_widget-5.blade.php
@@ -18,7 +18,7 @@
             <button type="button" class="btn btn-sm btn-icon btn-color-primary btn-active-light-primary" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
                 {!! theme()->getSvgIcon("icons/duotune/general/gen024.svg", "svg-icon-2") !!}
             </button>
-            {{ theme()->getView('partials/menus/_menu-3') }}
+            {{-- {{ theme()->getView('partials/menus/_menu-3') }} --}}
             <!--end::Menu-->
         </div>
     </div>
