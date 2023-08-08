728048ec9f6eab994050c38f43e8e1cef9388db2
islamshu12 <islamshu12@gmail.com>
2023-08-08 16:05:07 +0300

    edit

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -52,7 +52,8 @@ class HomeController extends BaseController
     public function testt(){
         $tags = Tag::where('blog_id',130)->get();
         foreach($tags as $tag){
-            $cleanedText = preg_replace('/[^A-Za-z0-9\s]/', 't', $tag->title);
+            $tage = str_replace('_',' ',$tag->title);
+            $cleanedText = preg_replace('/[^A-Za-z0-9\s]/', 't', $tage);
 
             dd($cleanedText);
         }
